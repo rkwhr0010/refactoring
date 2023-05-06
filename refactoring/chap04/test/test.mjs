a8b28d1b3461ad724c88032dcee3d9e28158157d
kks <rkwhr0010@naver.com>
2023-05-06 17:45:00 +0900

    올바른 테스트 방식

--- refactoring/chap04/test/test.mjs
+++ refactoring/chap04/test/test.mjs
@@ -3,7 +3,12 @@ import {Province, sampleProvinceData} from "../sample.mjs";
 import { expect} from "chai"; 
 
 describe('province', function(){
-    const asia = new Province(sampleProvinceData()); //테스트 간 상호작용을 하는 안좋은 예시
+    //좋은 예시
+    let asia;
+    beforeEach(function(){
+        asia = new Province(sampleProvinceData());
+    })
+    
     it('shortfall',function(){
         expect(asia.shortfall).equal(5);
     })
