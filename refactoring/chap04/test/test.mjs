3b26fca1c17bae4d144fb275108b3b9d6b6d9dae
kks <rkwhr0010@naver.com>
2023-05-06 17:40:57 +0900

    테스트끼리 상호작용하는 안좋은 예

--- refactoring/chap04/test/test.mjs
+++ refactoring/chap04/test/test.mjs
@@ -3,13 +3,12 @@ import {Province, sampleProvinceData} from "../sample.mjs";
 import { expect} from "chai"; 
 
 describe('province', function(){
+    const asia = new Province(sampleProvinceData()); //테스트 간 상호작용을 하는 안좋은 예시
     it('shortfall',function(){
-        const asia = new Province(sampleProvinceData()); 
         expect(asia.shortfall).equal(5);
     })
     //총 수익 계산
     it('profit', function(){
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).equal(230); 
     })
 })
