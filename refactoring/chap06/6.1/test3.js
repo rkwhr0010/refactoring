2e48b6471fde22d931e5872fdb84da00ee7255b2
kks <rkwhr0010@naver.com>
2023-05-07 00:48:44 +0900

    지역 변수의 값을 사용할때

---
+++ refactoring/chap06/6.1/test3.js
@@ -0,0 +1,32 @@
+//지역 변수를 사용할 때
+function printOwing(invoice){
+    let outstanding = 0;
+
+    printBanner();
+
+    //미해결 채무
+    for(const o of invoice.orders){
+        outstanding += o.amount;
+    }
+
+    recordDueDate(invoice);
+    //중첩 함수가 지원되지 않는 언어는 이렇게 넣어줘야 할 것
+    printDetails(invoice, outstanding);
+
+    function recordDueDate(invoice) {
+        const today = Clock.today;
+        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+    }
+    function printDetails(invoice, outstanding) {
+        console.log(`고객명 : ${invoice.customer}`);
+        console.log(`채무액 : ${outstanding}`);
+        console.log(`마감일 : ${invoice.dueDate.toLocaleDateString()}`);
+    }
+    function printBanner() {
+        console.log("******************");
+        console.log("**** 고객 채무 ****");
+        console.log("******************");
+    }
+}
+
+
