1926f71a261431b3883b65b29c40e535998f78a9
kks <rkwhr0010@naver.com>
2023-05-07 13:37:55 +0900

    6.2 함수인라인하기 기본형

---
+++ refactoring/chap06/6.2/test.js
@@ -0,0 +1,6 @@
+function rating(aDriver){
+    return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+function moreThanFiveLateDeliveries(aDriver){
+    return aDriver.numberOfLateDeliveres > 5;
+}
