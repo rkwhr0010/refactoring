3414ae6b9dad172e134f3114b3bdce6a7767ee83
kks <rkwhr0010@naver.com>
2023-05-07 13:59:06 +0900

    매개변수 이름이 다른 경우

--- refactoring/chap06/6.2/test.js
+++ refactoring/chap06/6.2/test.js
@@ -1,6 +1,7 @@
 function rating(aDriver){
     return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
 }
-function moreThanFiveLateDeliveries(aDriver){
-    return aDriver.numberOfLateDeliveres > 5;
+//매개변수 이름이 다른 경우
+function moreThanFiveLateDeliveries(dvr){
+    return dvr.numberOfLateDeliveres > 5;
 }
