1420a4f7d1dccf7c28fd79a1e403446186793018
kks <rkwhr0010@naver.com>
2023-05-07 20:25:39 +0900

    매개변수를 속성으로 바꾸기2

--- refactoring/chap06/6.5/test3_refac.js
+++ refactoring/chap06/6.5/test3_refac.js
@@ -1,6 +1,6 @@
-/* 현재 문제는 매개변수를 객체로 받고 있다. 재사용성이 제한된다.*/
 function inNewEngland(aCustomer){
-    return ["MA","CT","ME","VT","NH","RI"].includes(aCustomer.address.state);
+    const stateCode = aCustomer.address.state;
+    return ["MA","CT","ME","VT","NH","RI"].includes(stateCode);
 }
 //호출부
 const newEnglanders = someCustomers.filter(c=>inNewEngland(c));
