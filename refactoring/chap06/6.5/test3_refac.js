6f46af811f2e802f99018167d9bcd27782fcdff4
kks <rkwhr0010@naver.com>
2023-05-07 20:28:07 +0900

    매개변수를 속성으로 바꾸기3

--- refactoring/chap06/6.5/test3_refac.js
+++ refactoring/chap06/6.5/test3_refac.js
@@ -1,6 +1,11 @@
 function inNewEngland(aCustomer){
     const stateCode = aCustomer.address.state;
-    return ["MA","CT","ME","VT","NH","RI"].includes(stateCode);
+    return newFunction(stateCode);
+}
+//임시 함수 추출
+function newFunction(stateCode) {
+    return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 //호출부
 const newEnglanders = someCustomers.filter(c=>inNewEngland(c));
+
