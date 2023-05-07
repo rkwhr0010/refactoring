edb866bbe691c387d2107e7c37dd77b3a31c9548
kks <rkwhr0010@naver.com>
2023-05-07 20:10:39 +0900

    매개변수 추가하기 리팩터링2

--- refactoring/chap06/6.5/test2_refac.js
+++ refactoring/chap06/6.5/test2_refac.js
@@ -1,9 +1,11 @@
 class Book{
     addReservation(customer){
-        this.zz_addReservation(customer);
+        this.zz_addReservation(customer, false);
     }
-    //임시이름
-    zz_addReservation(customer) {
+    //매개변수 추가
+    zz_addReservation(customer,isPriority) {
+        //자바스크립트 한정 assert 테스트
+        assert(isPriority === true || isPriority === false);
         this._reservations.push(customer);
     }
 }
