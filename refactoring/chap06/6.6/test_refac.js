c190457c0030ae9c5364fc3f4e09a0e921a14caa
kks <rkwhr0010@naver.com>
2023-05-08 22:27:40 +0900

    6.6 변수 캡슐화하기 리팩터링3

--- refactoring/chap06/6.6/test_refac.js
+++ refactoring/chap06/6.6/test_refac.js
@@ -1,8 +1,8 @@
 let spaceship = {};
-let defaultOwner = {firstName:"마틴",lastName:"파울러"};
-
+//접근 제한자가 없는 언어에서는 이런 식으로 표현하는 것도 도움된다.
+let __privateOnly_defaultOwner = {firstName:"마틴",lastName:"파울러"};
 spaceship.owner = getDefaultOwner();
 setDefaultOwner({firstName:"레베카",lastName:"파슨스"});
-
-function getDefaultOwner(){return defaultOwner;}
-function setDefaultOwner(arg){return defaultOwner = arg;}
+//저자는 자바스크립트에서는 게터에 get을 빼는 것을 선호함
+function defaultOwner(){return __privateOnly_defaultOwner;}
+function setDefaultOwner(arg){return __privateOnly_defaultOwner = arg;}
