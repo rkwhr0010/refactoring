555773b309cb65c5de8f9dccb7954eba3b8faaa4
kks <rkwhr0010@naver.com>
2023-05-08 23:55:35 +0900

    6.6 값 캡슐화하기 게터에서 객체 복사본 리턴

--- refactoring/chap06/6.6/test_refac.mjs
+++ refactoring/chap06/6.6/test_refac.mjs
@@ -1,7 +1,6 @@
 let spaceship = {};
-//접근 제한자가 없는 언어에서는 이런 식으로 표현하는 것도 도움된다.
-let __privateOnly_defaultOwner = {firstName:"마틴",lastName:"파울러"};
+let defaultOwnerData = {firstName:"마틴",lastName:"파울러"};
 spaceship.owner = defaultOwner();
-//저자는 자바스크립트에서는 게터에 get을 빼는 것을 선호함
-export function defaultOwner(){return __privateOnly_defaultOwner;}
-export function setDefaultOwner(arg){__privateOnly_defaultOwner = arg;}
+//방법 1 게터에서 객체 복사본 리턴
+export function defaultOwner(){return Object.assign({},defaultOwnerData);}
+export function setDefaultOwner(arg){defaultOwnerData = arg;}
