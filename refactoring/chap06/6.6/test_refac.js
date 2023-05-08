915c7baf8a2739fdabbe3350a01ba17a1de4cf7c
kks <rkwhr0010@naver.com>
2023-05-08 22:23:23 +0900

    6.6 변수 캡슐화하기 리팩터링2

--- refactoring/chap06/6.6/test_refac.js
+++ refactoring/chap06/6.6/test_refac.js
@@ -1,7 +1,8 @@
 let spaceship = {};
 let defaultOwner = {firstName:"마틴",lastName:"파울러"};
-spaceship.owner = defaultOwner;
-defaultOwner = {firstName:"레베카",lastName:"파슨스"};
-//게터/세터 생성
+
+spaceship.owner = getDefaultOwner();
+setDefaultOwner({firstName:"레베카",lastName:"파슨스"});
+
 function getDefaultOwner(){return defaultOwner;}
 function setDefaultOwner(arg){return defaultOwner = arg;}
