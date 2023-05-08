fbc7f28149126e3a60d4749e84d3cb3215ed677f
kks <rkwhr0010@naver.com>
2023-05-08 23:51:31 +0900

    6.6 값 캡슐화하기

--- refactoring/chap06/6.6/test2_base.mjs
+++ refactoring/chap06/6.6/test2_base.mjs
@@ -4,4 +4,6 @@ const owner1 = defaultOwner();
 assert.equal("파울러",owner1.lastName,"처음 값 확인");
 const owner2 = defaultOwner();
 owner2.lastName ="파슨스";
-assert.equal("파슨스",owner2.lastName,"owner2 변경 후");
+console.log(owner1);
+assert.equal("파슨스",owner1.lastName,"owner2 변경 후");
+console.log(owner1);
