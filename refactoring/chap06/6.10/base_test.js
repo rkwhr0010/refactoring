1d1f51716f139c0176b7eb3a20a4d190f9927774
kks <rkwhr0010@naver.com>
2023-05-13 00:47:04 +0900

    완료

--- refactoring/chap06/6.10/base_test.js
+++ refactoring/chap06/6.10/base_test.js
@@ -9,7 +9,7 @@ const baseCharge = aReading.baseCharge;
 //client2.js  차 세금 일부 면제 코드
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+const taxableCharge = aReading.taxableCharge;
 
 //client3
 const rawReading = acquireReading(); 
