7507113193d749fb6d043b5920dce16be211a6e9
kks <rkwhr0010@naver.com>
2023-05-13 00:13:13 +0900

    객체 복사 함수 사용

--- refactoring/chap06/6.10/client3.js
+++ refactoring/chap06/6.10/client3.js
@@ -1,9 +1,13 @@
-/** client3.js 
- * 중복코드가 한 곳에 몰려있으면 함수 추출로 제거하면 된다.
- * 현재 중복 코드가 전체에 흩어져 있다고 가정한다.
- */
-const aReading = acquireReading();
+const rawReading = acquireReading(); //미가공 측정값
+const aReading = enrichReading(rawReading);
 const basicChargeAmount = calculateBaseCharge(aReading);
+
+//먼저 입력 객체를 그대로 복사해 반환하는 함수를 만든다.
+function enrichReading(original){
+    const result = _.cloneDeep(original);
+    return result;
+}
+
 //이미 함수로 만든 코드가 발견됐다.
 function calculateBaseCharge(aReading){
     return baseRate(aReading.month, aReading.year) * aReading.quantity;
