6a690d854a522eb968ed5b32717f8046f72424bf
kks <rkwhr0010@naver.com>
2023-05-13 00:15:48 +0900

    미가공 측정값에 기본 소비량을 부가 정보로 덧붙임

--- refactoring/chap06/6.10/client3.js
+++ refactoring/chap06/6.10/client3.js
@@ -1,10 +1,10 @@
-const rawReading = acquireReading(); //미가공 측정값
+const rawReading = acquireReading(); 
 const aReading = enrichReading(rawReading);
-const basicChargeAmount = calculateBaseCharge(aReading);
 
-//먼저 입력 객체를 그대로 복사해 반환하는 함수를 만든다.
 function enrichReading(original){
     const result = _.cloneDeep(original);
+    //미가공 측정값에 기본 소비량을 부가 정보로 덧붙임
+    result.baseCharge =  calculateBaseCharge(aReading);
     return result;
 }
 
