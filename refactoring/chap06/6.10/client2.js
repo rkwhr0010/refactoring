976ce7f9de861bbc2183994e35f70033662d857d
kks <rkwhr0010@naver.com>
2023-05-13 00:42:39 +0900

    client2 리팩토링중2

--- refactoring/chap06/6.10/client2.js
+++ refactoring/chap06/6.10/client2.js
@@ -1,5 +1,4 @@
 //client2.js  차 세금 일부 면제 코드
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const base = aReading.baseCharge;
-const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
