d5032de38a0b78808207cbbbcdf95f109a452dba
kks <rkwhr0010@naver.com>
2023-05-13 18:56:03 +0900

    상수 캡슐화하기

--- refactoring/chap07/7.1/ex1.js
+++ refactoring/chap07/7.1/ex1.js
@@ -1,6 +1,10 @@
 //이 상수는 레코드 구조로 사용되는 객체
-const organization = {name:"애크미 구스베리", country:"GB"};
+const organization = { name: "애크미 구스베리", country: "GB" };
 
+//상수를 캡슐화하기
+function getRawDataOfOrganization() {
+    return organization
+}
 let result;
-result += `<h1>${organization.name}</h1>` //일기
-organization.name = '새로운 이름' // 쓰기
+result += `<h1>${getRawDataOfOrganization().name}</h1>` //일기
+getRawDataOfOrganization().name = '새로운 이름' // 쓰기
