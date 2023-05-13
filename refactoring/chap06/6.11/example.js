ce41a5c522e56dd13e648240db6080e9d8b3e8c0
kks <rkwhr0010@naver.com>
2023-05-13 14:57:53 +0900

    상수 정리

--- refactoring/chap06/6.11/example.js
+++ refactoring/chap06/6.11/example.js
@@ -1,20 +1,16 @@
+//상수 정리
 function priceOrder(product, quantity, shippingMethod){
     const priceData = calculatePricingData(product, quantity);
-    const price = applyShipping(priceData, shippingMethod);
-    return price;
+    return applyShipping(priceData, shippingMethod);
 }
-//첫 번째 단계 처리 함수
 function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const priceData = { basePrice: basePrice, quantity: quantity, discount: discount };
-    return priceData;
+    return { basePrice: basePrice, quantity: quantity, discount: discount };
 }
-//두 번째 단계 처리 함수
 function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = priceData.quantity * shippingPerCase;
-    const price = priceData.basePrice - priceData.discount + shippingCost;
-    return price;
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
