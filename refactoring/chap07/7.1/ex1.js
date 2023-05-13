dde62db14e05c45b2c368d33ac0b971ca30f5e63
kks <rkwhr0010@naver.com>
2023-05-13 19:01:21 +0900

    레코드를 클래스로 바꾸기

--- refactoring/chap07/7.1/ex1.js
+++ refactoring/chap07/7.1/ex1.js
@@ -8,3 +8,10 @@ function getRawDataOfOrganization() {
 let result;
 result += `<h1>${getRawDataOfOrganization().name}</h1>` //일기
 getRawDataOfOrganization().name = '새로운 이름' // 쓰기
+//레코드를 캡슐화하는 목적은 변수를 통제하기 위함이다.
+//그러기 위해선 레코드를 클래스로 바꿔야한다.
+class Organization{
+    constructor(data){
+        this._data = data;
+    }
+}
