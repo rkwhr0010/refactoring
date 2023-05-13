b87beeea61bd334531cf550824891c0402e5640e
kks <rkwhr0010@naver.com>
2023-05-13 19:04:45 +0900

    레코드를 클래스로 바꾸기, 게터 추가

--- refactoring/chap07/7.1/ex1.js
+++ refactoring/chap07/7.1/ex1.js
@@ -1,13 +1,13 @@
 //이 상수는 레코드 구조로 사용되는 객체
-const organization = { name: "애크미 구스베리", country: "GB" };
-
+const organization = new Organization({ name: "애크미 구스베리", country: "GB" });
 //상수를 캡슐화하기, 이 게터는 임시 사용
-function getRawDataOfOrganization() {
-    return organization
-}
+function getRawDataOfOrganization() {return organization._data;}
+function getOrganization(){return organization;}
+
 let result;
 result += `<h1>${getRawDataOfOrganization().name}</h1>` //일기
 getRawDataOfOrganization().name = '새로운 이름' // 쓰기
+
 //레코드를 캡슐화하는 목적은 변수를 통제하기 위함이다.
 //그러기 위해선 레코드를 클래스로 바꿔야한다.
 class Organization{
