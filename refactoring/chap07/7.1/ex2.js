1a9359610d1e039d03bac1fb840c2bba4b26dfd9
kks <rkwhr0010@naver.com>
2023-05-13 20:16:33 +0900

    데이터 구조 안으로 들어가는 세터 함수화

--- refactoring/chap07/7.1/ex2.js
+++ refactoring/chap07/7.1/ex2.js
@@ -19,7 +19,12 @@ let customerData = {
         // 다른 고객 정보 생략...
     }
 }
-getRawDataOfCustomers()[customerID].usages[year][month] = amount;//쓰기
+setUsage(customerID, year, month, amount);//쓰기
+//데이터 구조 안으로 들어가는 세터 함수화
+function setUsage(customerID, year, month, amount) {
+    getRawDataOfCustomers()[customerID].usages[year][month] = amount;
+}
+
 function compareUsage(customerID, laterYear, month){//읽기
     const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
     const earlier = getRawDataOfCustomers()[customerID].usages[laterYear-1][month];
@@ -29,7 +34,6 @@ function getCustomerData(){return customerData;}
 function getRawDataOfCustomers(){return customerData._data;}//기존 호환용
 function setRawDataOfCustomers(arg){customerData = new CustomerData(arg);}
 
-//데이터 구조를 표햔하는 클래스 정의 후 이를 반환하는 함수 만듦
 class CustomerData{
     constructor(data){
         this._data = data;
