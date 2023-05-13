95c066e1581e20d6ba881ead6bfa27a6116f7e4a
kks <rkwhr0010@naver.com>
2023-05-13 15:52:39 +0900

    먼저, 두 번째 단계에 해당하는 코드를 메서드로 추출

--- refactoring/chap06/6.11/TestEx.java
+++ refactoring/chap06/6.11/TestEx.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-//자바 명령줄 프로그램 테스트 시 매번 JVM 구동은 느리니
-//JUnit 호출 가능한 상태로 변경
+
 public class TestEx {
 	static class Order{
 		String status;
@@ -20,9 +21,14 @@ public class TestEx {
 			System.exit(1);
 		}
 	}
+	//먼저, 두 번째 단계에 해당하는 코드를 메서드로 추출
 	static long run(String[] args) throws IOException{
 		if(args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
 		String filename = args[args.length -1];
+		return countOrders(args, filename);
+	}
+	private static long countOrders(String[] args, String filename)
+			throws IOException, StreamReadException, DatabindException {
 		File input = Paths.get(filename).toFile();
 		ObjectMapper mapper = new ObjectMapper();
 		Order[] orders = mapper.readValue(input, Order[].class);
