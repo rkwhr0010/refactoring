71c8b379951c5ab852ce1ed7c694243cb1dbfa0f
kks <rkwhr0010@naver.com>
2023-05-13 14:39:45 +0900

    중간 데이터 구조

--- refactoring/chap06/6.11/example.js
+++ refactoring/chap06/6.11/example.js
@@ -1,11 +1,12 @@
-//배송비 계산 부분 함수로 추출
+//단계 간 주고받을 중간 데이터 구조
 function priceOrder(product, quantity, shippingMethod){
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+    const priceData = {};
+    const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
     return price;
 }
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
+function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingPerCase;
