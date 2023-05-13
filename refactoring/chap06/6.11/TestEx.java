688186717d465989e5c029d2d583395602bd9788
kks <rkwhr0010@naver.com>
2023-05-13 16:11:52 +0900

    중간 데이토 구조로 옮김, 이후 첫번째 단계로 옮김, 이후 매개변수 제거

--- refactoring/chap06/6.11/TestEx.java
+++ refactoring/chap06/6.11/TestEx.java
@@ -20,20 +20,24 @@ public class TestEx {
 			System.exit(1);
 		}
 	}
-	//중간 데이터 구조 추가
-	private static class CommandLine{}
+	private static class CommandLine{
+		private boolean onlyCountReady;
+	}
 	static long run(String[] args) throws IOException{
 		if(args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
 		CommandLine commandLine = new CommandLine();
 		String filename = args[args.length -1];
-		return countOrders(commandLine, args, filename);
+		//중간 데이토 구조로 옮김, 이후 첫번째 단계로 옮김, 이후 매개변수 제거
+		commandLine.onlyCountReady = Stream.of(args).anyMatch(arg->"-r".equals(arg));
+		return countOrders(commandLine, filename);
 	}
-	private static long countOrders(CommandLine commandLine, String[] args, String filename)
+	private static long countOrders(CommandLine commandLine, String filename)
 			throws IOException {
 		File input = Paths.get(filename).toFile();
 		ObjectMapper mapper = new ObjectMapper();
 		Order[] orders = mapper.readValue(input, Order[].class);
-		if(Stream.of(args).anyMatch(arg->"-r".equals(arg))) {
+
+		if(commandLine.onlyCountReady) {
 			return Stream.of(orders)
 					.filter(o->"ready".equals(o.status))
 					.count();
