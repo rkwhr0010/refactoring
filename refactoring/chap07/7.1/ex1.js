4b217315bf5269d337f379368a9ee1b9b333f9d3
kks <rkwhr0010@naver.com>
2023-05-13 19:19:54 +0900

    레코드 연결 끊기

--- refactoring/chap07/7.1/ex1.js
+++ refactoring/chap07/7.1/ex1.js
@@ -1,16 +1,19 @@
 const organization = new Organization({ name: "애크미 구스베리", country: "GB" });
-// function getRawDataOfOrganization() {return organization._data;} 정상동작하면 제거
 function getOrganization(){return organization;}
 
 let result;
 result += `<h1>${getOrganization().name}</h1>` //일기
 getOrganization().name = '새로운 이름' // 쓰기
-
-//게터, 세터 생성 후 사용하도록 코드 수정
+//현재 레코드를 그대로 받아 사용하고 있다.
+//레코드는 참조변수로 다루기에 캡슐화가 깨질 우려가 있다. 제거한다.
+//만일 _data를 그대로 쓸 것이라면 복제해서 써야한다.
 class Organization{
     constructor(data){
-        this._data = data;
+        this._name = data.name;
+        this._country = data.country;
     }
-    set name(aString){this._data.name = aString;}
-    get name(){return this._data.name;}
+    set name(aString){this._name = aString;}
+    get name(){return this._name;}
+    set _country(aString){this._country = aString;}
+    get _country(){return this._country;}
 }
