0409d559c3632340f1e933912ca518aff2e02ca5
kks <rkwhr0010@naver.com>
2023-05-13 23:04:04 +0900

    캡슐화 실수할 수 있는 케이스

--- refactoring/chap07/7.2/ex1.js
+++ refactoring/chap07/7.2/ex1.js
@@ -15,8 +15,15 @@ class Course {
     get name(){return this._name;}
     get isAdvanced(){return this._isAdvanced;}
 }
-//클라이언트 Person이 제공하는 수업 컬렉션에서 수업 정보 얻음
+
 let numAdvancedCourses;
 numAdvancedCourses = aPerson.courses
     .filter(c=>c.isAdvanced)
     .length;
+
+
+//클라이언트
+const basicCourseNames = readBasicCourseNames(filename);
+aPerson.courses = basicCourseNames.map(name => new Course(name, false));
+//Person에서 게터로 수업컬렉션을 못가져오지만
+//내가 수업 컬렉션을 세터로 설정하면, 수업 컬렉션을 수정해 캡슐화가 깨질 수 있다.
