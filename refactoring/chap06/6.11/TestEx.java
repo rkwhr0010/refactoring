dc94335651056fd936c7363ec60a9f7df7aac9f0
kks <rkwhr0010@naver.com>
2023-05-13 16:00:16 +0900

    중간 데이터 구조 추가

--- refactoring/chap06/6.11/TestEx.java
+++ refactoring/chap06/6.11/TestEx.java
@@ -5,14 +5,13 @@ import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestEx {
 	static class Order{
 		String status;
 	}
+	
 	public static void main(String[] args) {
 		try {
 			System.out.println(run(args));
@@ -21,14 +20,16 @@ public class TestEx {
 			System.exit(1);
 		}
 	}
-	//먼저, 두 번째 단계에 해당하는 코드를 메서드로 추출
+	//중간 데이터 구조 추가
+	private static class CommandLine{}
 	static long run(String[] args) throws IOException{
 		if(args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+		CommandLine commandLine = new CommandLine();
 		String filename = args[args.length -1];
-		return countOrders(args, filename);
+		return countOrders(commandLine, args, filename);
 	}
-	private static long countOrders(String[] args, String filename)
-			throws IOException, StreamReadException, DatabindException {
+	private static long countOrders(CommandLine commandLine, String[] args, String filename)
+			throws IOException {
 		File input = Paths.get(filename).toFile();
 		ObjectMapper mapper = new ObjectMapper();
 		Order[] orders = mapper.readValue(input, Order[].class);
