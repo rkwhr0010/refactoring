92f4dd3ebb7435846283e0c145907ea9a8108dec
kks <rkwhr0010@naver.com>
2023-05-13 14:32:47 +0900

    배송비 계산 부분 함수로 추출

--- refactoring/chap06/6.11/example.js
+++ refactoring/chap06/6.11/example.js
@@ -1,8 +1,13 @@
+//배송비 계산 부분 함수로 추출
 function priceOrder(product, quantity, shippingMethod){
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold )
-        ?shippingMethod.discountedFee : shippingMethod.feePerCase;
+    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+    return price;
+}
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
+    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+        ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingPerCase;
     const price = basePrice - discount + shippingCost;
     return price;
