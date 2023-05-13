3019374cc04c84b604d105fb81fbc8d8f76de934
kks <rkwhr0010@naver.com>
2023-05-13 14:41:46 +0900

    중간 데이터 구조에 데이터를 넣고 인자를 하나씩 줄여간다

--- refactoring/chap06/6.11/example.js
+++ refactoring/chap06/6.11/example.js
@@ -1,15 +1,15 @@
-//단계 간 주고받을 중간 데이터 구조
+//중간 데이터 구조에 데이터를 넣고 인자를 하나씩 줄여간다.
 function priceOrder(product, quantity, shippingMethod){
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const priceData = {};
+    const priceData = {basePrice:basePrice};
     const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
     return price;
 }
-function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
-    const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
+function applyShipping(priceData, shippingMethod, quantity, discount) {
+    const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingPerCase;
-    const price = basePrice - discount + shippingCost;
+    const price = priceData.basePrice - discount + shippingCost;
     return price;
 }
