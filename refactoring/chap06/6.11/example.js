0c14eb9775d7faf2c4f666b93ce486b770aaef52
kks <rkwhr0010@naver.com>
2023-05-13 14:46:57 +0900

    중간 데이터 구조에 데이터를 넣고 인자를 하나씩 줄여간다.

--- refactoring/chap06/6.11/example.js
+++ refactoring/chap06/6.11/example.js
@@ -3,7 +3,7 @@ function priceOrder(product, quantity, shippingMethod){
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
     const priceData = {basePrice:basePrice};
-    const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
+    const price = applyShipping(priceData, shippingMethod, quantity, discount);
     return price;
 }
 function applyShipping(priceData, shippingMethod, quantity, discount) {
