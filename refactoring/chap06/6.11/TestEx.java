c3e48e1518b8a640e848c93603ca1144b00e89cb
kks <rkwhr0010@naver.com>
2023-05-13 15:47:59 +0900

    JUnit 호출 가능한 상태로 변경

--- refactoring/chap06/6.11/TestEx.java
+++ refactoring/chap06/6.11/TestEx.java
@@ -1,33 +1,37 @@
 package kimspring.helloboot;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+//자바 명령줄 프로그램 테스트 시 매번 JVM 구동은 느리니
+//JUnit 호출 가능한 상태로 변경
 public class TestEx {
-	//의미없음
 	static class Order{
 		String status;
 	}
 	public static void main(String[] args) {
 		try {
-			if(args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
-			String filename = args[args.length -1];
-			File input = Paths.get(filename).toFile();
-			ObjectMapper mapper = new ObjectMapper();
-			Order[] orders = mapper.readValue(input, Order[].class);
-			if(Stream.of(args).anyMatch(arg->"-r".equals(arg))) {
-				System.out.println(Stream.of(orders)
-						.filter(o->"ready".equals(o.status))
-						.count());
-			}else {
-				System.out.println(orders.length);
-			}
+			System.out.println(run(args));
 		} catch (Exception e) {
 			System.err.println(e);
 			System.exit(1);
+		}
+	}
+	static long run(String[] args) throws IOException{
+		if(args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+		String filename = args[args.length -1];
+		File input = Paths.get(filename).toFile();
+		ObjectMapper mapper = new ObjectMapper();
+		Order[] orders = mapper.readValue(input, Order[].class);
+		if(Stream.of(args).anyMatch(arg->"-r".equals(arg))) {
+			return Stream.of(orders)
+					.filter(o->"ready".equals(o.status))
+					.count();
+		}else {
+			return orders.length;
 		}
 	}
 }
