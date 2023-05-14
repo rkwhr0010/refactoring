079add58b9dad97269e9f55c2b119d1165d6e7a4
kks <rkwhr0010@naver.com>
2023-05-14 13:43:33 +0900

    우선순위 자체가 아닌 우선순위를 표현하는 문자열 표현

--- refactoring/chap07/7.3/ex1.js
+++ refactoring/chap07/7.3/ex1.js
@@ -1,8 +1,8 @@
+//우선순위 자체가 아닌 우선순위를 표현하는 문자열 표현
 class Order{
     constructor(data){
         this._priority = data.priority;
     }
-    //정의한 클래스 사용하도록 변경
     get priorityString(){return this._priority.toString();}
     set priority(aString){this._priority = new Priority(aString);}
 }
@@ -10,12 +10,9 @@ class Priority{
     constructor(value){
         this._value = value;
     }
-    //get value() 가 아닌 변환함수 사용
     toString(){return this._value;}
 }
 
-
-//클라이언트
 let highPriorityCount = orders.filter(o => "high" === o.priorityString
                                         || "rush" === o.priorityString)
                               .length;
