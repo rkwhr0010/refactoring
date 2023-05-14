864e80050dfecaf4f80031f899f9c3827d196797
kks <rkwhr0010@naver.com>
2023-05-14 22:22:27 +0900

    조건문 함수로 추출

--- refactoring/chap10/10.1/ex1.js
+++ refactoring/chap10/10.1/ex1.js
@@ -1,6 +1,10 @@
-//여름철엔 할인율이 달라짐
-if(!aDate.isBefore(plan.summerStrat) && !aDate.isAfter(plan.summerEnd)){
+if(summer()){
     charge = quantity * plan.summerRate;
 }else{
     charge = quantity * plan.regularRate + plan.regularServiceCharge;
 }
+//추출
+function summer() {
+    return !aDate.isBefore(plan.summerStrat) && !aDate.isAfter(plan.summerEnd);
+}
+
