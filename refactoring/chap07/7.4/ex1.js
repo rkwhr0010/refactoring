fc99d48736c678d9fa20d30615a19f40739764cc
kks <rkwhr0010@naver.com>
2023-05-14 14:16:03 +0900

    게터로 추출

--- refactoring/chap07/7.4/ex1.js
+++ refactoring/chap07/7.4/ex1.js
@@ -4,9 +4,14 @@ class Order {
         this._item = item;
     }
     get price(){
-        const basePrice = this._quantity * this._item.price;
+        const basePrice = this.basePrice();
         var discountFactor = 0.98;
         if(basePrice>1000) discountFactor -= 0.03;
         else basePrice * discountFactor;
+
+    }
+    //게터로 추출
+    get basePrice() {
+        return this._quantity * this._item.price;
     }
 }
