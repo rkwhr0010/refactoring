652bfec23d42f7d802fd34aa7df4aa863a297929
kks <rkwhr0010@naver.com>
2023-05-14 22:33:24 +0900

    10.2 조건식 통합하기

---
+++ refactoring/chap10/10.2/ex1.js
@@ -0,0 +1,7 @@
+//장애 수당 계산
+function disabilityAmount(anEmployee){
+    if(anEmployee.seniority < 2) return 0;
+    if(anEmployee.monthsDisabled > 12) return 0;
+    if(anEmployee.isPartTime) return 0;
+    //...
+}
