0b346d67d8f3d1a60b28829195a2f973d6a3e0ce
kks <rkwhr0010@naver.com>
2023-05-14 16:57:43 +0900

    7.6 클래스 인라인하기

--- refactoring/chap07/7.6/ex1.js
+++ refactoring/chap07/7.6/ex1.js
@@ -1,9 +1,9 @@
 //현재 제일을 못하는 클래스라 가정
 class TrackingInformation{
     get shippingCompany(){return this._shippingCompany;}
-    set shippingCompany(arg){return this._shippingCompany = arg;}
+    set shippingCompany(arg){this._shippingCompany = arg;}
     get trackingNumber(){return this._trackingNumber;}
-    set trackingNumber(arg){return this._trackingNumber = arg;}
+    set trackingNumber(arg){ his._trackingNumber = arg;}
     get display(){
         return `${this.shippingCompany} : ${this.trackingNumber}`;
     }
@@ -13,8 +13,12 @@ class Shipment{
     get trackingInfo(){
         return this._trackingInformation.display;
     }
+    
+
     get trackingInformation() {return this._trackingInformation;}
     set trackingInformation(aTrackingInformation) {
         this._trackingInformation = aTrackingInformation;
     }
 }
+//클라이언트
+aShipment.trackingInformation.shippingCompany = request.vender;
