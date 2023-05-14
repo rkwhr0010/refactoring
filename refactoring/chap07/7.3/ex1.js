c1022c11be36635cb99a84f1e05642225e12e85e
kks <rkwhr0010@naver.com>
2023-05-14 13:37:50 +0900

    기본형 값 클래스 정의

--- refactoring/chap07/7.3/ex1.js
+++ refactoring/chap07/7.3/ex1.js
@@ -2,10 +2,18 @@ class Order{
     constructor(data){
         this._priority = data.priority;
     }
-    //변수부터 캡슐화
     get priority(){return this._priority;}
     set priority(arg){this._priority = arg;}
 }
+//우선순위 속성을 표현하는 값 클래스 정의
+class Priority{
+    constructor(value){
+        this._value = value;
+    }
+    toString(){return this._value;}
+}
+
+
 //클라이언트
 let highPriorityCount = orders.filter(o => "high" === o.priority
                                         || "rush" === o.priority)
