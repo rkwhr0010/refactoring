293fad5196e03d13ffa04f17085bb0fa436518e5
kks <rkwhr0010@naver.com>
2023-05-14 15:23:21 +0900

    추출할 클래스 정의 기존 클래스 생성자에서 그 클래스 인스턴스 저장해두기

--- refactoring/chap07/7.5/ex1.js
+++ refactoring/chap07/7.5/ex1.js
@@ -1,4 +1,9 @@
 class Person{
+    constructor(){
+        //인스턴스 저장해 두기
+        this._telphoneNumber = new TelephoneNumber();
+    }
+
     get name(){return this._name;}
     set name(arg){this._name = arg;}
     get telephoneNumber(){return `(${this.officeAreaCode}) ${this.officeNumber}`;}
@@ -6,4 +11,8 @@ class Person{
     set officeAreaCode(arg){this._officeAreaCode = arg;}
     get officeNumber(){return this._officeNumber;}
     set officeNumber(arg){return this._officeNumber = arg;}
+}
+//추출할 클래스 정의
+class TelephoneNumber{
+
 }
