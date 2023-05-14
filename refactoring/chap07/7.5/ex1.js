e579b0540b82c7e6974f8d4bf0c50d0237212a35
kks <rkwhr0010@naver.com>
2023-05-14 16:14:12 +0900

    나머지 게터/세터 정의 및 사용

--- refactoring/chap07/7.5/ex1.js
+++ refactoring/chap07/7.5/ex1.js
@@ -4,14 +4,16 @@ class Person{
     }
     get name(){return this._name;}
     set name(arg){this._name = arg;}
-    get telephoneNumber(){return `(${this.officeAreaCode}) ${this.officeNumber}`;}
+    get telephoneNumber(){return this._telphoneNumber.telephoneNumber;}
     get officeAreaCode(){return this._telphoneNumber.officeAreaCode;}
     set officeAreaCode(arg){this._telphoneNumber.officeAreaCode = arg;}
-    get officeNumber(){return this._officeNumber;}
-    set officeNumber(arg){return this._officeNumber = arg;}
+    get officeNumber(){return this._telphoneNumber.officeNumber;}
+    set officeNumber(arg){return this._telphoneNumber.officeNumber = arg;}
 }
-//게터/세터 정의 및 사용 
 class TelephoneNumber{
     get officeAreaCode(){return this._officeAreaCode;}
     set officeAreaCode(arg){return this._officeAreaCode = arg;}
+    get officeNumber(){return this._officeNumber;}
+    set officeNumber(arg){return this._officeNumber = arg;}
+    get telephoneNumber(){return `(${this.officeAreaCode}) ${this.officeNumber}`;}
 }
