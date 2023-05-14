9824ed04224b84b75d353ebf0d986692c2fcfd3c
kks <rkwhr0010@naver.com>
2023-05-14 13:33:46 +0900

    변수부터 캡슐화

--- refactoring/chap07/7.3/ex1.js
+++ refactoring/chap07/7.3/ex1.js
@@ -1,7 +1,10 @@
 class Order{
     constructor(data){
-        this.priority = data.priority;
+        this._priority = data.priority;
     }
+    //변수부터 캡슐화
+    get priority(){return this._priority;}
+    set priority(arg){this._priority = arg;}
 }
 //클라이언트
 let highPriorityCount = orders.filter(o => "high" === o.priority
