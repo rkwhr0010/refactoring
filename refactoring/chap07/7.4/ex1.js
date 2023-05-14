ea2a1f34c0b42cdc762c604bba265400202da8c1
kks <rkwhr0010@naver.com>
2023-05-14 14:14:03 +0900

    var const로 바꿔보기 봇보고 지나진 대입문을 찾을 수 있다문

--- refactoring/chap07/7.4/ex1.js
+++ refactoring/chap07/7.4/ex1.js
@@ -4,7 +4,7 @@ class Order {
         this._item = item;
     }
     get price(){
-        var basePrice = this._quantity * this._item.price;
+        const basePrice = this._quantity * this._item.price;
         var discountFactor = 0.98;
         if(basePrice>1000) discountFactor -= 0.03;
         else basePrice * discountFactor;
