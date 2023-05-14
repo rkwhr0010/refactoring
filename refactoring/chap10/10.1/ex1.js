4b9731775a936263e0c00c1be6bd1116956d9825
kks <rkwhr0010@naver.com>
2023-05-14 22:25:03 +0900

    추가 리팩터링

--- refactoring/chap10/10.1/ex1.js
+++ refactoring/chap10/10.1/ex1.js
@@ -1,9 +1,5 @@
-//나머지도 정리
-if(summer()){
-    charge = summerCharge();
-}else{
-    charge = regularCharge();
-}
+//조건문을 함수로 정리하니 이렇게 추가로 정리할 기회를 얻었다.
+charge = summer() ? summerCharge() : regularCharge();
 function regularCharge() {
     return quantity * plan.regularRate + plan.regularServiceCharge;
 }
