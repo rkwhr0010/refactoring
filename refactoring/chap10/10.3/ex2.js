102fd7b40eeeb4260bec0c0881e6bc0a27a9dea3
kks <rkwhr0010@naver.com>
2023-05-14 23:33:40 +0900

    조건식 통

--- refactoring/chap10/10.3/ex2.js
+++ refactoring/chap10/10.3/ex2.js
@@ -1,10 +1,9 @@
 function adjustedCapital(anInstrument){
     let result = 0;
-    if(anInstrument.capital <= 0){ return result;}
-    //not 조건 제거
-    if(anInstrument.interestRate <= 0 || anInstrument.duration <= 0){
-        return result;
-    }
+    //조건식 통합
+    if(anInstrument.capital <= 0 
+        || anInstrument.interestRate <= 0 
+        || anInstrument.duration <= 0){ return result;}
     result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
     return result
 }
