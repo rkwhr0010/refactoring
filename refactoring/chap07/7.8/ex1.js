47cc8676ab812704f46c1ddd43f52a87ed0ca0cd
kks <rkwhr0010@naver.com>
2023-05-14 19:20:05 +0900

    7.8 중개자 제거하기 완료

--- refactoring/chap07/7.8/ex1.js
+++ refactoring/chap07/7.8/ex1.js
@@ -2,7 +2,6 @@
 let manager = aPerson.department.manager;
 
 class Person{
-    get manager(){return this._department.manager;}
     //게터 추가 후 클라이언트 코드 수정 후 위임(중개) 메서드 제거
     get department(){return this._department;}
 }
