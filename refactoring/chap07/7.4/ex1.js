71648e4a8534c4229ce3a098cee2df0a2adc3922
kks <rkwhr0010@naver.com>
2023-05-14 14:18:11 +0900

    const 변경

--- refactoring/chap07/7.4/ex1.js
+++ refactoring/chap07/7.4/ex1.js
@@ -4,8 +4,7 @@ class Order {
         this._item = item;
     }
     get price(){
-        var discountFactor = 0.98;
-        //변수 인라인
+        const discountFactor = 0.98;
         if(this.basePrice>1000) discountFactor -= 0.03;
         else this.basePrice * discountFactor;
     }
