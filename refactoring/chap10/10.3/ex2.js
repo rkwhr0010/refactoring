21a3f6c9a3047a461b0a8282bf47b3cc8739a372
kks <rkwhr0010@naver.com>
2023-05-14 23:32:45 +0900

    not 제거

--- refactoring/chap10/10.3/ex2.js
+++ refactoring/chap10/10.3/ex2.js
@@ -1,7 +1,8 @@
 function adjustedCapital(anInstrument){
     let result = 0;
     if(anInstrument.capital <= 0){ return result;}
-    if(!(anInstrument.interestRate > 0 && anInstrument.duration > 0)){
+    //not 조건 제거
+    if(anInstrument.interestRate <= 0 || anInstrument.duration <= 0){
         return result;
     }
     result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
