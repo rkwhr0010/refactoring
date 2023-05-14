83180dc16bc2e5519052edb50fe752eac1c04c91
kks <rkwhr0010@naver.com>
2023-05-14 17:08:28 +0900

    나머지도 옮긴 후 클래스 TrackingInformation 제거

--- refactoring/chap07/7.6/ex1.js
+++ refactoring/chap07/7.6/ex1.js
@@ -1,26 +1,21 @@
-//현재 제일을 못하는 클래스라 가정
-class TrackingInformation{
-    get shippingCompany(){return this._shippingCompany;}
-    set shippingCompany(arg){this._shippingCompany = arg;}
-    get trackingNumber(){return this._trackingNumber;}
-    set trackingNumber(arg){ his._trackingNumber = arg;}
-    get display(){
-        return `${this.shippingCompany} : ${this.trackingNumber}`;
-    }
-}
-
 class Shipment{
     get trackingInfo(){
         return this._trackingInformation.display;
     }
-    get shippingCompany(){return this._trackingInformation.shippingCompany;}
-    set shippingCompany(arg){this._trackingInformation.shippingCompany = arg;}
+    get display(){
+        return `${this.shippingCompany} : ${this.trackingNumber}`;
+    }
+    //나머지도 옮긴 후 클래스 TrackingInformation 제거
+    get shippingCompany(){return this._shippingCompany;}
+    set shippingCompany(arg){this._shippingCompany = arg;}
+    get display(){
+        return `${this.shippingCompany} : ${this.trackingNumber}`;
+    }
 
     get trackingInformation() {return this._trackingInformation;}
     set trackingInformation(aTrackingInformation) {
         this._trackingInformation = aTrackingInformation;
     }
 }
-//클라이언트- 호출부분에서 TrackingInformation 제거
 let aShipment = new Shipment();
 aShipment.shippingCompany = request.vender;
