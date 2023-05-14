0b407e23475e0c96b6023d605917946de6fa11f7
kks <rkwhr0010@naver.com>
2023-05-14 16:18:52 +0900

    새로 만든 클래스 이름 정리, 이 클래스는 순수한 전화번호이므로 사무실 이름을 제거 및 정리

--- refactoring/chap07/7.5/ex1.js
+++ refactoring/chap07/7.5/ex1.js
@@ -4,16 +4,17 @@ class Person{
     }
     get name(){return this._name;}
     set name(arg){this._name = arg;}
-    get telephoneNumber(){return this._telphoneNumber.telephoneNumber;}
-    get officeAreaCode(){return this._telphoneNumber.officeAreaCode;}
-    set officeAreaCode(arg){this._telphoneNumber.officeAreaCode = arg;}
-    get officeNumber(){return this._telphoneNumber.officeNumber;}
-    set officeNumber(arg){return this._telphoneNumber.officeNumber = arg;}
+    get telephoneNumber(){return this._telphoneNumber.toString();}
+    get officeAreaCode(){return this._telphoneNumber.areaCode;}
+    set officeAreaCode(arg){this._telphoneNumber.areaCode = arg;}
+    get officeNumber(){return this._telphoneNumber.number;}
+    set officeNumber(arg){return this._telphoneNumber.number = arg;}
 }
+//새로 만든 클래스 이름 정리, 이 클래스는 순수한 전화번호이므로 사무실 이름을 제거 및 정리
 class TelephoneNumber{
-    get officeAreaCode(){return this._officeAreaCode;}
-    set officeAreaCode(arg){return this._officeAreaCode = arg;}
-    get officeNumber(){return this._officeNumber;}
-    set officeNumber(arg){return this._officeNumber = arg;}
-    get telephoneNumber(){return `(${this.officeAreaCode}) ${this.officeNumber}`;}
+    get areaCode(){return this._areaCode;}
+    set areaCode(arg){return this._areaCode = arg;}
+    get number(){return this.number;}
+    set number(arg){return this.number = arg;}
+    toString(){return `(${this.areaCode}) ${this.number}`;}
 }
