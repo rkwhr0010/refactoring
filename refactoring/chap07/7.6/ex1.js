9de361996a32014b516d252b52091468a76b6822
kks <rkwhr0010@naver.com>
2023-05-14 17:05:30 +0900

    클라이언트- 호출부분에서 TrackingInformation 제거

--- refactoring/chap07/7.6/ex1.js
+++ refactoring/chap07/7.6/ex1.js
@@ -13,12 +13,14 @@ class Shipment{
     get trackingInfo(){
         return this._trackingInformation.display;
     }
-    
+    get shippingCompany(){return this._trackingInformation.shippingCompany;}
+    set shippingCompany(arg){this._trackingInformation.shippingCompany = arg;}
 
     get trackingInformation() {return this._trackingInformation;}
     set trackingInformation(aTrackingInformation) {
         this._trackingInformation = aTrackingInformation;
     }
 }
-//클라이언트
-aShipment.trackingInformation.shippingCompany = request.vender;
+//클라이언트- 호출부분에서 TrackingInformation 제거
+let aShipment = new Shipment();
+aShipment.shippingCompany = request.vender;
