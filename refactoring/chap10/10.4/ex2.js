9c51264933a1cb8a597588e275d855c7d795bc7d
kks <rkwhr0010@naver.com>
2023-05-15 23:15:59 +0900

    vogageAndHistoryLengthFactor 추가 함수 정의 이 함수 오버라이딩으로 voyageProfitFactor 깔끔하게 분리

--- refactoring/chap10/10.4/ex2.js
+++ refactoring/chap10/10.4/ex2.js
@@ -39,20 +39,8 @@ class Rating{
     }
     get vogageAndHistoryLengthFactor {
         let result = 0;
-        if (this.voyage.zone === "중국" && this.hasChinaHistory) {
-            result += 3;
-            if (this.history.length > 10)
-                result += 1;
-            if (this.voyage.length > 12)
-                result += 1;
-            if (this.voyage.length > 18)
-                result -= 1;
-        } else {
-            if (this.history.length > 8)
-                result += 1;
-            if (this.voyage.length > 14)
-                result -= 1;
-        }
+        if (this.history.length > 8) result += 1;
+        if (this.voyage.length > 14) result -= 1;
         return result;
     }
 
@@ -65,6 +53,14 @@ class ExperiencedChinaRating extends Rating{
     get captainHistoryRisk(){
         const result = super.captainHistoryRisk - 2; 
         return Math.max(result,0);
+    }
+    get vogageAndHistoryLengthFactor {
+        let result = 0;
+        result += 3;
+        if (this.history.length > 10) result += 1;
+        if (this.voyage.length > 12) result += 1;
+        if (this.voyage.length > 18) result -= 1;
+        return result;
     }
 }
 //팩터리 함수
