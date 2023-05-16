a18a1333d06285873675652518520fb55052a008
kks <rkwhr0010@naver.com>
2023-05-16 22:37:19 +0900

    예시: 객체 리터럴 이용하기

---
+++ refactoring/chap10/10.5/ex2.js
@@ -0,0 +1,27 @@
+//고객 정보를 갱신하는 클라이언트가 없음
+class Site{
+    get customer(){return this._customer;}
+}
+class Customer{
+    get name(){}
+    get billingPlan(){}
+    set billingPlan(arg){}
+    get paymentHistory(){}
+}
+function client1(){
+    const aCustomer = site.customer;
+    /*코드 들*/
+    let customerName;
+    if(aCustomer === "미확인 고객") customerName = "거주자";
+    else customerName = aCustomer.name;
+}
+function client2(){
+    const plan = (aCustomer === "미확인 고객")
+        ? CustomElementRegistry.billingPlans.basic
+        : aCustomer.billingPlan;
+}
+function client3(){
+    const weeksDelinquent = (aCustomer === "미확인 고객")
+        ? 0
+        : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+}
