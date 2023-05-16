bc2013fe6188932a1d22e3b34df89d1de7c26eb7
kks <rkwhr0010@naver.com>
2023-05-16 23:24:40 +0900

    특이 케이스 조건 검사 부분 함수로 추출, 및 사용

--- refactoring/chap10/10.5/ex2.js
+++ refactoring/chap10/10.5/ex2.js
@@ -15,20 +15,25 @@ function createUnknownCustomer(){
         isUnknown : true,
     };
 }
+//특이 케이스 조건 검사 부분 함수로 추출, 및 사용
+function isUnknown(arg){
+    return (arg === "미확인 고객");
+}
+
 function client1(){
     const aCustomer = site.customer;
     /*코드 들*/
     let customerName;
-    if(aCustomer === "미확인 고객") customerName = "거주자";
+    if(isUnknown(aCustomer)) customerName = "거주자";
     else customerName = aCustomer.name;
 }
 function client2(){
-    const plan = (aCustomer === "미확인 고객")
+    const plan = isUnknown(aCustomer)
         ? CustomElementRegistry.billingPlans.basic
         : aCustomer.billingPlan;
 }
 function client3(){
-    const weeksDelinquent = (aCustomer === "미확인 고객")
+    const weeksDelinquent = isUnknown(aCustomer)
         ? 0
         : aCustomer.paymentHistory.weeksDelinquentInLastYear;
 }
