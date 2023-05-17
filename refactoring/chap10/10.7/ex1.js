4e508f12b175a90f2c0eb136788a2c7774e75083
kks <rkwhr0010@naver.com>
2023-05-17 22:01:17 +0900

    함수로 추출

--- refactoring/chap10/10.7/ex1.js
+++ refactoring/chap10/10.7/ex1.js
@@ -1,12 +1,16 @@
 function eaxmple(){
+    checkForMiscreants(people);
+}
+//함수로 추출
+function checkForMiscreants(people) {
     let found = false;
-    for(const p of people){
-        if( ! found){
-            if(p === "조커"){
+    for (const p of people) {
+        if (!found) {
+            if (p === "조커") {
                 sendAlert();
                 found = true;
             }
-            if(p === "사루만"){
+            if (p === "사루만") {
                 sendAlert();
                 found = true;
             }
