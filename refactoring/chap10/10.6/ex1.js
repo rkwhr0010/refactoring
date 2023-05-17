73eac014119086f9374d0ac46949c8cf8c387aee
kks <rkwhr0010@naver.com>
2023-05-17 21:16:26 +0900

    어셔션 적합한 위치로 옮김

--- refactoring/chap10/10.6/ex1.js
+++ refactoring/chap10/10.6/ex1.js
@@ -1,9 +1,12 @@
 class Customer {
     applyDiscount(aNumber){
         if(!this.discountRate) return aNumber;
-        else {
-            assert(this.discountRate>=0);
-            return aNumber - (this.discountRate * aNumber);
-        }
+        else return aNumber - (this.discountRate * aNumber);
+    }
+    //문제 범위를 좁히기 위해 세터에 옮겼다.
+    //applyDiscount에서 통과 못했을 때 추가로 추적을 해야 하기 때문이다.
+    set discountRate(aNumber){
+        assert(null === aNumber || aNumber >= 0);
+        this._discountRate = aNumber;
     }
 }
