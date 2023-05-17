36c3198c36a50add5c2ae2cabc894ff0e09756e0
kks <rkwhr0010@naver.com>
2023-05-17 22:10:55 +0900

    더가다듬기

--- refactoring/chap10/10.7/ex1.js
+++ refactoring/chap10/10.7/ex1.js
@@ -1,16 +1,7 @@
 function eaxmple(){
     checkForMiscreants(people);
 }
-//함수로 추출
+//더 가다듬기
 function checkForMiscreants(people) {
-    for (const p of people) {
-        if (p === "조커") {
-            sendAlert();
-            return;
-        }
-        if (p === "사루만") {
-            sendAlert();
-            return;
-        }
-    }
+    if(people.some(p=>["조커","사루만"].includes(p))) sendAlert();
 }
