4d102dc1a2e9414cc5145e45d031fa6f4673e3f0
kks <rkwhr0010@naver.com>
2023-05-17 21:55:39 +0900

    10.7 제어 플래그 탈출문으로 바꾸기

---
+++ refactoring/chap10/10.7/ex1.js
@@ -0,0 +1,15 @@
+function eaxmple(){
+    let found = false;
+    for(const p of people){
+        if( ! found){
+            if(p === "조커"){
+                sendAlert();
+                found = true;
+            }
+            if(p === "사루만"){
+                sendAlert();
+                found = true;
+            }
+        }
+    }
+}
