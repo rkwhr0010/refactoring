604ea51133567f4110693db5b7a6bc53c6bd935b
kks <rkwhr0010@naver.com>
2023-05-21 20:29:14 +0900

    예시:조건문이 있을 때의 슬라이드

---
+++ refactoring/chap08/8.6/ex2.js
@@ -0,0 +1,11 @@
+function example(){
+    let result;
+    if(availableResources.length === 0){
+        result = createResource();
+        allocatedResources.push(result);
+    }else{
+        result = availableResources.pop();
+        allocatedResources.push(result);
+    }
+    return result;
+}
