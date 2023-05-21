e489869a58382cf5fa6f383c51816c97d43ea878
kks <rkwhr0010@naver.com>
2023-05-21 18:57:57 +0900

    원래 emitPhotoData는 제거 후 개 함수 emitPhotoData로 변경

--- refactoring/chap08/8.4/ex1.js
+++ refactoring/chap08/8.4/ex1.js
@@ -1,7 +1,7 @@
 function renderPerson(outStream, person){
     outStream.write(`<p>${person.name}</p>\n`);
     renderPhoto(outStream, person.photo);
-    zztmp(outStream, person.photo);
+    emitPhotoData(outStream, person.photo);
     outStream.write(`<p>위치: ${photo.location}</p>\n`);
 }
 function listRecentPhotos(outStream, photos){
@@ -10,15 +10,11 @@ function listRecentPhotos(outStream, photos){
         .forEach(p=>{
             outStream.write("<div>\n");
             emitPhotoData(outStream, p)
+            outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
             outStream.write("</div>\n");
         });
 }
-function emitPhotoData(outStream, photo){
-    zztmp(outStream, photo);
-    outStream.write(`<p>위치: ${photo.location}</p>\n`);
-}
-
-function zztmp(outStream, photo) {
+function emitPhotoData(outStream, photo) {
     outStream.write(`<p>제목: ${photo.title}</p>\n`);
     outStream.write(`<p>날짜: ${photo.date.toDateString()}</p>\n`);
 }
