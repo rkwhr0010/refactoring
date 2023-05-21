6b1d512923e7db4360fb72ffde3b4445d90de2f0
kks <rkwhr0010@naver.com>
2023-05-21 22:41:56 +0900

    더다듬기

--- refactoring/chap08/8.8/ex1.js
+++ refactoring/chap08/8.8/ex1.js
@@ -1,11 +1,10 @@
 function acquireData(input){
     const lines = input.split("\n");
-    const result = lines
+    return lines
         .slice(1)
         .filter(line => line.trim !== "")
         .map(line => line.split(","))
         .filter(record=>record[1].trim() === "India")
         .map(result => ({city:record[0].trim(), phone:record[2].trim()}))
         ;
-    return result;
 }
