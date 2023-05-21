a1112734d6e198095f5136643d79b189fb4187c1
kks <rkwhr0010@naver.com>
2023-05-21 22:33:07 +0900

    로직을 진적으로 대체하면서 테스트한다.

--- refactoring/chap08/8.8/ex1.js
+++ refactoring/chap08/8.8/ex1.js
@@ -1,16 +1,12 @@
 function acquireData(input){
     const lines = input.split("\n");
-    // let firstLine = true;
     const result = [];
     //로직을 진적으로 대체하면서 테스트한다.
     const loopItems = lines
-        .slice(1);
+        .slice(1)
+        .filter(line => line.trim !== "")
+        ;
     for(const line of loopItems){
-        // if(firstLine){
-        //     firstLine = false;
-        //     continue;
-        // }
-        if(line.trim() === "") continue;
         const record = line.split(",");
         if(record[1].trim() === "India"){
             result.push({city:record[0].trim(), phone:record[2].trim()})
