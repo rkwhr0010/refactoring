7d3602e738123eec769c43b9f41fed58d7a5f3b1
kks <rkwhr0010@naver.com>
2023-05-21 21:18:04 +0900

    알맞은 리팩터링 수행

--- refactoring/chap08/8.7/ex1.js
+++ refactoring/chap08/8.7/ex1.js
@@ -1,18 +1,11 @@
 function example(){
     return `최연소:${youngestAge()}, 총 급여: ${totalSalary()}`;
-    //각각 함수로 추출
+    //알고리즘 교체하기
     function youngestAge() {
-        let youngest = people[0] ? people[0].age : Infinity;
-        for (const p of people)
-            if (p.age < youngest)
-                youngest = p.age;
-        return youngest;
+        return people.map(p=>p.age).reduce((a1,a2)=>a1<a2?a1:a2, Infinity);
     }
-
+    //파이프라인으로 바꾸기
     function totalSalary() {
-        let totalSalary = 0;
-        for (const p of people)
-            totalSalary += p.salary;
-        return totalSalary;
+        return people.reduce((total, p)=> total+ p.salary,0);;
     }
 }
