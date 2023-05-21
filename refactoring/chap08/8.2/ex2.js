47fc45115a5d682466b67289e010de8f257c4540
kks <rkwhr0010@naver.com>
2023-05-21 15:53:48 +0900

    완료

--- refactoring/chap08/8.2/ex2.js
+++ refactoring/chap08/8.2/ex2.js
@@ -2,13 +2,9 @@ class Account{
     constructor(number, type, interestRate){
         this._number = number;
         this._type = type;
-        //어셔션 추가한 상태로 당분간 운영하기
-        assert(interestRate === this._type.interestRate);
-        this._interestRate = interestRate;
     }
-    get interestRate(){return this._interestRate;}
+    get interestRate(){return this._type.interestRate;}
 }
-//interestRate 이자율 필드 생성 및 접근자 추가
 class AccountType{
     constructor(nameString, interestRate){
         this._name = nameString;
