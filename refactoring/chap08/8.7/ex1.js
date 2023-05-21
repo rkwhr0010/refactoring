c8ba759a08dd117fc12b7c8d142b5a34ff8e8574
kks <rkwhr0010@naver.com>
2023-05-21 21:06:25 +0900

    각각 함수로 추출

--- refactoring/chap08/8.7/ex1.js
+++ refactoring/chap08/8.7/ex1.js
@@ -1,8 +1,18 @@
 function example(){
-    let totalSalary = 0;
-    for(const p of people) totalSalary += p.salary;
-    //더 나아가 각 반복문을 각 함수로 만들기, 먼저 근처로 문장 슬라이드
-    let youngest = people[0] ? people[0].age : Infinity;
-    for(const p of people) if(p.age<youngest) youngest = p.age;
-    return `최연소:${youngest}, 총 급여: ${totalSalary}`;
+    return `최연소:${youngestAge()}, 총 급여: ${totalSalary()}`;
+    //각각 함수로 추출
+    function youngestAge() {
+        let youngest = people[0] ? people[0].age : Infinity;
+        for (const p of people)
+            if (p.age < youngest)
+                youngest = p.age;
+        return youngest;
+    }
+
+    function totalSalary() {
+        let totalSalary = 0;
+        for (const p of people)
+            totalSalary += p.salary;
+        return totalSalary;
+    }
 }
