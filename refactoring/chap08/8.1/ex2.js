624e1a4949bc7eafce4a6cd21645537af45f2905
kks <rkwhr0010@naver.com>
2023-05-21 14:59:03 +0900

    위임 메서드로 수정

--- refactoring/chap08/8.1/ex2.js
+++ refactoring/chap08/8.1/ex2.js
@@ -4,22 +4,11 @@ class Account{
         if(this._daysOverdrawn > 0) result += this.overdraftCharge;
         return result
     }
-    get overdraftCharge(){
-        if(this.type.isPremium){
-            const baseCharge = 10;
-            if(this.daysOverdrawn <= 7){
-                return baseCharge;
-            }else{
-                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
-            }
-        }else{
-            return this.daysOverdrawn * 1.75;
-        }
+    get overdraftCharge(){//위임 메서드
+        return this.type.overdraftCharge(this.daysOverdrawn);
     }
 }
 class AccountType{
-    //계좌 타입으로 복사 후 적절하게 수정
-    //daysOverdrawn는 계좌별로 달라지므로 원래 클래스에 남겨두었다.
     overdraftCharge(daysOverdrawn){
         if(this.isPremium){
             const baseCharge = 10;
