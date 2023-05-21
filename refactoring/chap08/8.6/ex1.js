223c157638920b4377c1b27f249a0306d7b1d95a
kks <rkwhr0010@naver.com>
2023-05-21 20:26:50 +0900

    가로막히는 경우

--- refactoring/chap08/8.6/ex1.js
+++ refactoring/chap08/8.6/ex1.js
@@ -1,15 +1,13 @@
-//사이드 이펙트가 없는 코드끼리는 마음대로 재배치 할 수 있다.
-//부수효과가 없는 프로그래밍을 하는 이유
 const pricingPlan = retrievePricingPlan();
 const baseCharge = pricingPlan.base;
 let charge;
 const chargePerUnit = pricingPlan.unit;
-const order = retreiveOrder(); // 바로 근처로 옮김
+const order = retreiveOrder();
 const units = order.units;
 charge = baseCharge + units * chargePerUnit;
 let discountableUnits = Math.max(units - princingPlan * discountThreshold, 0);
-let discount; //바로 근처로 옮김
+let discount; 
 discount = discountableUnits * pricingPlan.discountFactor;
-if(order.isRepeat) discount += 20;
+if(order.isRepeat) discount += 20; //이 줄은 맨끝으로 슬라이드하고 싶어도 참조하는 라인이 있어 막힌다.
 charge = charge - discount;
 chargeOrder(charge);
