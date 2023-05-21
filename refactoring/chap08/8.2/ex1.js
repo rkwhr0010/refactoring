be03fe623f18518bec3f388c344bab47ce951855
kks <rkwhr0010@naver.com>
2023-05-21 15:28:55 +0900

    CustomerContract 필드와 접근자 추가

--- refactoring/chap08/8.2/ex1.js
+++ refactoring/chap08/8.2/ex1.js
@@ -16,8 +16,12 @@ class Customer{
         return amount.subtract(amount.multiply(this._discountRate));
     }
 }
+//필드와 접근자 추가
 class CustomerContract{
-    constructor(startDate){
+    constructor(startDate,discountRate){
         this._startDate = startDate;
+        this._discountRate = discountRate;
     }
+    get discountRate(){return this._discountRate;}
+    set discountRate(arg){this._discountRate = arg;}
 }
