ddb56834361e09632577ed40e027e9369dcd9a83
kks <rkwhr0010@naver.com>
2023-05-21 14:28:00 +0900

    distance() , redians() 함수 calculateDistance 문맥으로 옮김

--- refactoring/chap08/8.1/ex1.js
+++ refactoring/chap08/8.1/ex1.js
@@ -14,21 +14,23 @@ function trackSummary(points){
             result += distance(points[i-1], points[i]);
         }
         return result;
+        //distance, radians 는 현재 문맥에서 calculateDistance에서만 사용중
+        //따라서 calculateDistance()함수 안으로 옮기는게 적절하다.
+        function distance(p1, p2) {
+            const EARTH_RADIUS = 3959; // 단위 마일
+            const dLat = radians(p2.lat) - radians(p1.lat);
+            const dLon = radians(p2.lon) - radians(p1.lon);
+            const a = Math.pow(Math.sin(dLat / 2), 2)
+                + Math.cos(radians(p2.lat))
+                * Math.cos(radians(p1.lat))
+                * Math.pow(Math.sin(dLon /2), 2);
+            const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            return EARTH_RADIUS * c;
+        } 
+        function radians(degrees) {
+            return degrees * Math.PI / 180;
+        } 
     }
-    function distance(p1, p2) {
-        const EARTH_RADIUS = 3959; // 단위 마일
-        const dLat = radians(p2.lat) - radians(p1.lat);
-        const dLon = radians(p2.lon) - radians(p1.lon);
-        const a = Math.pow(Math.sin(dLat / 2), 2)
-            + Math.cos(radians(p2.lat))
-            * Math.cos(radians(p1.lat))
-            * Math.pow(Math.sin(dLon /2), 2);
-        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        return EARTH_RADIUS * c;
-    } 
-    function radians(degrees) {
-        return degrees * Math.PI / 180;
-    } 
     function calculateTime() {/* 총 시간 계산 */} 
 }
 function top_calculateDistance(points) { 
