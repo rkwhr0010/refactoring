283d5b5ef9ae00b121adb3709b124ca22283bdc1
kks <rkwhr0010@naver.com>
2023-05-21 21:03:47 +0900

    더 나아가 각 반복문을 각 함수로 만들기, 먼저 근처로 문장 슬라이드

--- refactoring/chap08/8.7/ex1.js
+++ refactoring/chap08/8.7/ex1.js
@@ -1,9 +1,8 @@
 function example(){
-    let youngest = people[0] ? people[0].age : Infinity;
     let totalSalary = 0;
-    //부수효과가 있는 코드는 한쪽만 남기고 제거
     for(const p of people) totalSalary += p.salary;
+    //더 나아가 각 반복문을 각 함수로 만들기, 먼저 근처로 문장 슬라이드
+    let youngest = people[0] ? people[0].age : Infinity;
     for(const p of people) if(p.age<youngest) youngest = p.age;
-    
     return `최연소:${youngest}, 총 급여: ${totalSalary}`;
 }
