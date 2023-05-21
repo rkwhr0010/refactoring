a5ff9bdc5e7bee6412426b3fbb5ef513a699836c
kks <rkwhr0010@naver.com>
2023-05-21 15:24:47 +0900

    대상 변수 캡슐화

--- refactoring/chap08/8.2/ex1.js
+++ refactoring/chap08/8.2/ex1.js
@@ -1,10 +1,13 @@
 class Customer{
     constructor(name, discountRate){
         this._name = name;
-        this._discountRate = discountRate;
+        this._setDiscountRate(discountRate);
         this._contract = new CustomerContract(dateToday());
     }
     get discountRate(){return this._discountRate;}
+    //먼저 변수 캡슐화부터
+    _setDiscountRate(aNumber){this._discountRate = aNumber;}
+
     becomePreferred(){
         this._discountRate += 0.03;
         //코드들 ...
