05b7189ed69653f70a7bd901bcb54f9e5d4933b3
kks <rkwhr0010@naver.com>
2023-05-21 21:00:30 +0900

    부수효과가 있는 코드는 한쪽만 남기고 제거

--- refactoring/chap08/8.7/ex1.js
+++ refactoring/chap08/8.7/ex1.js
@@ -1,14 +1,9 @@
 function example(){
     let youngest = people[0] ? people[0].age : Infinity;
     let totalSalary = 0;
-    for(const p of people){
-        if(p.age<youngest) youngest = p.age;
-        totalSalary += p.salary;
-    }
-    //먼저 단순한 복사
-    for(const p of people){
-        if(p.age<youngest) youngest = p.age;
-        totalSalary += p.salary;
-    }
+    //부수효과가 있는 코드는 한쪽만 남기고 제거
+    for(const p of people) totalSalary += p.salary;
+    for(const p of people) if(p.age<youngest) youngest = p.age;
+    
     return `최연소:${youngest}, 총 급여: ${totalSalary}`;
 }
