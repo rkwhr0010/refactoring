87e0e4fd3ce4613bc60ef8cb9d8760f05410d132
kks <rkwhr0010@naver.com>
2023-05-21 18:30:54 +0900

    함수 이름 바꾸기로 마무리

--- refactoring/chap08/8.3/ex.js
+++ refactoring/chap08/8.3/ex.js
@@ -2,19 +2,18 @@ function renderPerson(outStream, person){
     const result = [];
     result.push(`<p>${person.name}</p>`);
     result.push(renderPhoto(person.photo));
-    //추가 중복 제거
-    result.push(zznew(person.photo));
+    result.push(emitPhotoData(person.photo));
     return result.join("\n");
 }
 function photoDiv(p){
     return [
         "<div>",
-        zznew(p),
+        emitPhotoData(p),
         "</div>",
     ].join("\n");
 }
-//emitPhotoData()인라인하기
-function zznew(p){
+//함수 이름 바꾸기로 마무리
+function emitPhotoData(p){
     return [
         `<p>제목: ${p.title}</p>`,
         `<p>위치: ${p.location}</p>` ,
