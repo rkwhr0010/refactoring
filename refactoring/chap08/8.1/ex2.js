e204872f67321922402855ac36b6cc04659f3d63
kks <rkwhr0010@naver.com>
2023-05-21 14:57:09 +0900

    overdraftCharge()함수 옮기고 적절히 수정

--- refactoring/chap08/8.1/ex2.js
+++ refactoring/chap08/8.1/ex2.js
@@ -1,11 +1,11 @@
 class Account{
-    get bankCharge(){ //은행이자
+    get bankCharge(){ 
         let result = 4.5;
         if(this._daysOverdrawn > 0) result += this.overdraftCharge;
         return result
     }
     get overdraftCharge(){
-        if(this.type.isPremium){//초과 인출 이자 계산
+        if(this.type.isPremium){
             const baseCharge = 10;
             if(this.daysOverdrawn <= 7){
                 return baseCharge;
@@ -14,6 +14,22 @@ class Account{
             }
         }else{
             return this.daysOverdrawn * 1.75;
+        }
+    }
+}
+class AccountType{
+    //계좌 타입으로 복사 후 적절하게 수정
+    //daysOverdrawn는 계좌별로 달라지므로 원래 클래스에 남겨두었다.
+    overdraftCharge(daysOverdrawn){
+        if(this.isPremium){
+            const baseCharge = 10;
+            if(this.daysOverdrawn <= 7){
+                return baseCharge;
+            }else{
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+            }
+        }else{
+            return daysOverdrawn * 1.75;
         }
     }
 }
