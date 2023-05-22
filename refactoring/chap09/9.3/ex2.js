c1b8ee165e08287b410ec0fa8f5b54597ff34e18
kks <rkwhr0010@naver.com>
2023-05-22 22:05:55 +0900

    완료

--- refactoring/chap09/9.3/ex2.js
+++ refactoring/chap09/9.3/ex2.js
@@ -4,7 +4,14 @@ class ProductionPlan {
         this._productionAccumulator = 0;
         this._adjustments = [];
     }
-    get production(){return this._initialProduction + this._productionAccumulator;}
+    get production(){
+        assert(this._productionAccumulator + this.calculatedProductionAccumulator);
+        return this._initialProduction + this._productionAccumulator;
+    }
+    get calculatedProductionAccumulator(){
+        return this._adjustments
+        .reduce((sum, a)=>sum+a.amount,0);
+    }
     applyAdjustment(anAdjustment){
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amout;
