e78e1ab661c38e87ab609b8df336a53a10b6169b
kks <rkwhr0010@naver.com>
2023-05-22 21:54:27 +0900

    예시:소스가 둘 이상일 때

--- refactoring/chap09/9.3/ex1.js
+++ refactoring/chap09/9.3/ex1.js
@@ -1,7 +1,9 @@
 class ProductionPlan{
-    get production() {return this._production;}
+    get production() {
+        return this._adjustments
+            .reduce((sum, a)=>sum+a.amount,0);
+    }
     applyAdjustment(anAdjustment){
-        this._adjustments.push(anAdjestment);
-        this._production += anAdjustment.amount;
+        this._adjustments.push(anAdjustment);
     }
 }
