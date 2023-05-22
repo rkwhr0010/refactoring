dcf6048a222b5c944335127c8a178cd5e37715c0
kks <rkwhr0010@naver.com>
2023-05-22 20:58:09 +0900

    생성자 조정

--- refactoring/chap09/9.2/ex1.js
+++ refactoring/chap09/9.2/ex1.js
@@ -1,8 +1,8 @@
 const organization = {name: "애크미 구스베리",country:"GB"};
 class Organization{
     constructor(data){
-        //생성자와 접근자를 구분하여 점진적으로 변경
-        this._title = data.name;
+        //생성자 조정
+        this._title = (data.title !== undefined) ? data.title : data.name;
         this._country = data.country;
     }
     get name(){return this._title;}
