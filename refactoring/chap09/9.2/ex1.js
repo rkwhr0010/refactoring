de53203bac2965ed70d829722501c2ac477765b4
kks <rkwhr0010@naver.com>
2023-05-22 20:56:52 +0900

    생성자와 접근자를 구분하여 점진적으로 변경

--- refactoring/chap09/9.2/ex1.js
+++ refactoring/chap09/9.2/ex1.js
@@ -1,13 +1,12 @@
 const organization = {name: "애크미 구스베리",country:"GB"};
-//organization 레코드 클래스로 캡슐화
 class Organization{
     constructor(data){
-        this._name = data.name;
+        //생성자와 접근자를 구분하여 점진적으로 변경
+        this._title = data.name;
         this._country = data.country;
     }
-    get name(){return this._name;}
-    set name(arg){ this._name = arg;}
+    get name(){return this._title;}
+    set name(arg){ this._title = arg;}
     get country(){return this._country;}
     set country(arg){ this._country = arg;}
-    
 }
