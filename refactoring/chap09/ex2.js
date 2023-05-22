9d75da1042ce5a706a3c1f142913fc8ad222937f
kks <rkwhr0010@naver.com>
2023-05-22 20:20:22 +0900

    적절한 이름으로 변경

--- refactoring/chap09/ex2.js
+++ refactoring/chap09/ex2.js
@@ -1,7 +1,7 @@
-function discount(originalInputValue, quantity){
-    //call by value 로 가정, 값을 복사해서 쓴다.
-    let inputValue = originalInputValue;
-    if(inputValue > 50) inputValue = inputValue - 2;
-    if(quantity > 100) inputValue = inputValue - 1;
-    return inputValue;
+function discount(inputValue, quantity){
+    //적절한 이름으로 변경
+    let result = inputValue;
+    if(inputValue > 50) result = result - 2;
+    if(quantity > 100) result = result - 1;
+    return result;
 }
