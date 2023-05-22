c608f16a6278569f48f18dc9e6f03f3ec55763d2
kks <rkwhr0010@naver.com>
2023-05-22 20:52:47 +0900

    organization 레코드 클래스로 캡슐화

--- refactoring/chap09/9.2/ex1.js
+++ refactoring/chap09/9.2/ex1.js
@@ -1,1 +1,13 @@
 const organization = {name: "애크미 구스베리",country:"GB"};
+//organization 레코드 클래스로 캡슐화
+class Organization{
+    constructor(data){
+        this._name = data.name;
+        this._country = data.country;
+    }
+    get name(){return this._name;}
+    set name(arg){ this._name = arg;}
+    get country(){return this._country;}
+    set country(arg){ this._country = arg;}
+    
+}
