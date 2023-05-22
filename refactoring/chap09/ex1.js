cbc03cba52325349ce58805bb251dcb33e68685e
kks <rkwhr0010@naver.com>
2023-05-22 20:06:27 +0900

    불변으로 정의

--- refactoring/chap09/ex1.js
+++ refactoring/chap09/ex1.js
@@ -1,12 +1,14 @@
 function distanceTravelled(scenario, time){
     let result;
-    let acc = scenario.primaryForce / scenario.mass;//가속도=힘/질량
+    //불변으로 정의
+    const primaryAcceleration = scenario.primaryForce / scenario.mass;
     let primaryTime = Math.min(time, scenario.delay);
-    result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+    result = 0.5 * primaryAcceleration * primaryTime * primaryTime; 
     let secondaryTime = time - scenario.delay;
     if(secondaryTime > 0){
-        let primaryVelocity = acc * scenario.delay;
-        acc = (scenario.primaryForce + scenario.secondaryForce) /scenario.mass;
+        let primaryVelocity = primaryAcceleration * scenario.delay;
+        //별도 변수
+        let acc = (scenario.primaryForce + scenario.secondaryForce) /scenario.mass;
         result += primaryVelocity * secondaryTime + 0.5 * acc * secondaryTime *secondaryTime;
     }
     return result;
