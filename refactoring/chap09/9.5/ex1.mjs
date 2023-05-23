1458a1caf7c781e4f7f0826acc952baf6d52a214
kks <rkwhr0010@naver.com>
2023-05-23 20:33:29 +0900

    완료

--- refactoring/chap09/9.5/ex1.mjs
+++ refactoring/chap09/9.5/ex1.mjs
@@ -1,9 +1,10 @@
+/* ex1.mjs */
 import {findCustomer, initialize,registerCustomer,_repositoryData} from './repository.mjs';
 
 export class Order {
     constructor(data){
         this._number = data.number;
-        this._customer = new Customer(data.customer);
+        this._customer = registerCustomer(data.customer);
         //여러 데이터...
     }
     get customer(){return this._customer;}
@@ -15,10 +16,20 @@ export class Customer{
         this._id = id;
     }
     get id(){return this._id;}
+    set id(arg){this._id = arg;}
 }
 initialize();
-registerCustomer("100");
-registerCustomer("200");
-registerCustomer("100");
-console.log(findCustomer("100"));
+
+
+const customer1 = new Customer("10");
+const orders = [];
+
+orders.push(new Order({number:1 , customer:"10"}));
+orders.push(new Order({number:2 , customer:"10"}));
+orders.push(new Order({number:3 , customer:"10"}));
+orders.push(new Order({number:4 , customer:"10"}));
+
 console.log(_repositoryData);
+orders.forEach(o=>console.log(o));
+findCustomer("10").id = "20";
+orders.forEach(o=>console.log(o));
--- refactoring/chap09/9.5/repository.mjs
+++ refactoring/chap09/9.5/repository.mjs
@@ -1,3 +1,4 @@
+/*repository.mjs */
 import {Order, Customer} from './ex1.mjs'
 //물리적으로 똑같은 객체를 저장할 곳
 export let _repositoryData;//저장소 객체
