1a09da63b679580e53bd5fc2366899886dc67228
kks <rkwhr0010@naver.com>
2023-05-30 07:48:37 +0900

    원래 함수 호출하던 곳, 새로 만든 질의함수로 바꾸기

--- refactoring/chap11/ex01.js
+++ refactoring/chap11/ex01.js
@@ -11,7 +11,6 @@ function alertForMiscreant(people){
     }
     return "";
 }
-//부수효과 제거
 function findMiscreant(people){
     for(const p of people){
         if(p==="조커") return "조커";
@@ -19,3 +18,6 @@ function findMiscreant(people){
     }
     return "";
 }
+//원래 함수 호출하던 곳, 새로 만든 질의함수로 바꾸기
+// const found = alertForMiscreant(people);
+const found = findMiscreant(people);
