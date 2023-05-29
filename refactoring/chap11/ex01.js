f6869783065ddf1d137750cdde3d588b3e07aac4
kks <rkwhr0010@naver.com>
2023-05-30 07:45:06 +0900

    함수 복제 후 질의 목적에 맞는 이름 짓기

--- refactoring/chap11/ex01.js
+++ refactoring/chap11/ex01.js
@@ -11,3 +11,17 @@ function alertForMiscreant(people){
     }
     return "";
 }
+//함수 복제 후 질의 목적에 맞는 이름 짓기
+function findMiscreant(people){
+    for(const p of people){
+        if(p==="조커"){
+            setOffAlarms();
+            return "조커";
+        }
+        if(p==="사루만"){
+            setOffAlarms();
+            return "사루만";
+        }
+    }
+    return "";
+}
