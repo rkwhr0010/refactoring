d1180e0ce094556740b0bc3e6852426542267e59
kks <rkwhr0010@naver.com>
2023-05-31 07:30:03 +0900

    기존 함수 호출하는 곳 새 함수 호출로 변경

--- refactoring/chap11/11.4/ex1.js
+++ refactoring/chap11/11.4/ex1.js
@@ -1,15 +1,11 @@
 function example(){
     //기존 함수 호출하는 곳 새 함수 호출로 변경
-    if(!aPlan.xxNEWwithinRange(aRoom.datsTempRange))
+    if(!aPlan.withinRange(aRoom.datsTempRange))
         alerts.push("방온도가 지정 범위를 벗어났습니다.");
 }
-
 class HeatingPlan{
-    withinRange(bottom, top){
-        return (bottom >= this._temperatureRange.low)
-            && (top <= this._temperatureRange.high);
-    }
-    xxNEWwithinRange(aNumberRange){
-        return this.withinRange(aNumberRange.low, aNumberRange.high);
+    withinRange(aNumberRange){
+        return (aNumberRange.low >= this._temperatureRange.low)
+            && (aNumberRange.high <= this._temperatureRange.high);
     }
 }
