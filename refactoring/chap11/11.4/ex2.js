81621a880ce4e4cdd2d6d129deea7c7133758903
kks <rkwhr0010@naver.com>
2023-05-31 07:41:31 +0900

    변수 추출하기

--- refactoring/chap11/11.4/ex2.js
+++ refactoring/chap11/11.4/ex2.js
@@ -1,6 +1,8 @@
 function example(){
-    const low = aRoom.daysTempRange.low;
-    const hihg = aRoom.daysTempRange.high;
-    if(!aPlan.withinRange(low,high))
+    const tempRange = aRoom.daysTempRange;
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = aPlan.withinRange(low, high);
+    if(!isWithinRange)
         alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 }
