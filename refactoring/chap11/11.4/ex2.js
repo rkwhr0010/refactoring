15b3f515b45d9862359a15e3ea52f1dd851f8379
kks <rkwhr0010@naver.com>
2023-05-31 07:43:05 +0900

    함수 추출하기

--- refactoring/chap11/11.4/ex2.js
+++ refactoring/chap11/11.4/ex2.js
@@ -1,8 +1,13 @@
 function example(){
     const tempRange = aRoom.daysTempRange;
-    const low = tempRange.low;
-    const high = tempRange.high;
-    const isWithinRange = aPlan.withinRange(low, high);
-    if(!isWithinRange)
-        alerts.push("방 온도가 지정 범위를 벗어났습니다.");
+    const isWithinRange = xxNEWwithinRange();
+    //함수 추출하기
+    function xxNEWwithinRange() {
+        const low = tempRange.low;
+        const high = tempRange.high;
+        const isWithinRange = aPlan.withinRange(low, high);
+        if (!isWithinRange)
+            alerts.push("방 온도가 지정 범위를 벗어났습니다.");
+        return isWithinRange;
+    }
 }
