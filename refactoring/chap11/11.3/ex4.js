0532a10c85b265d039fb929270b25c42dc72d751
kks <rkwhr0010@naver.com>
2023-05-30 22:40:22 +0900

    이후 원래플래그 함수를 호출하지 말라는 의미에서 함수 이름 변경

--- refactoring/chap11/11.3/ex4.js
+++ refactoring/chap11/11.3/ex4.js
@@ -1,23 +1,23 @@
 //지나치게 까다로워 고치려면 일이 커질 것 같으면 래핑하는 함수도 고려할만 하다.
-function rushDeliveryDate(anOrder) {return deliveryDate(anOrder, true);}
-function regularDeliveryDate(anOrder) {return deliveryDate(anOrder, false);}
+function rushDeliveryDate(anOrder) {return deliveryDateHelperOnly(anOrder, true);}
+function regularDeliveryDate(anOrder) {return deliveryDateHelperOnly(anOrder, false);}
 
-function deliveryDate(anOrder, isRush){
+function deliveryDateHelperOnly(anOrder, isRush){
     let result;
     let deliveryTime;
-    if(anOrder.deliveryState === "MA" || anOrder.deliveryState === "CT")
+    
+    if(["MA","CT"].includes(anOrder.deliveryState))
         deliveryTime = isRush ? 1 : 2;
-    else if (anOrder.deliveryState === "NY" || anOrder.deliveryState === "NH"){
+    else if ( ["NY","NH"].includes(anOrder.deliveryState)){
         deliveryTime = 2;
         if(anOrder.deliveryState === "NH" && !isRush)
             deliveryTime = 3;
     }
-    else if (isRush)
-        deliveryTime = 3;
-    else if (anOrder.deliveryState === "ME")
+    else if (isRush || anOrder.deliveryState === "ME")
         deliveryTime = 3;
     else 
         deliveryTime = 4;
+
     result = anOrder.placeOn.plusDays(2 + deliveryTime);
     if(isRush) result = result.minusDays(1);
     return result;
