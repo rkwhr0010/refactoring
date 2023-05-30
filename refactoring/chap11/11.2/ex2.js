9b4aef0f66cbdadf4df5f5953f8b3980bd4f3623
kks <rkwhr0010@naver.com>
2023-05-30 20:06:44 +0900

    통합할 함수를 선정할 때 보통 중간 함수가 적절

--- refactoring/chap11/11.2/ex2.js
+++ refactoring/chap11/11.2/ex2.js
@@ -1,15 +1,17 @@
 function baseCharge(usage){
     if(usage < 0) return usd(0);
     const amount = bottomBand(usage) * 0.03
-        + middleBand(usage) * 0.05
+        + withinBand(usage, 100, 200) * 0.05
         + topBand(usage) * 0.07;
     return usd(amount);
 }
 function bottomBand(usage){
     return Math.min(usage, 100);
 }
-function middleBand(usage){
-    return usage > 100 ? Math.min(usage,200) - 100 : 0;
+//통합할 함수를 선정할 때 보통 중간 함수가 적절
+//매개변수화 하여 넣기
+function withinBand(usage,bottom,top){
+    return usage > bottom ? Math.min(usage,top) - bottom : 0;
 }
 function topBand(usage){
     return usage > 200 ? usage - 200 : 0;
