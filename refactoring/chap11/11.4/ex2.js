06b1ade309f2d6d6ca4a92f5464bd2c35cd96170
kks <rkwhr0010@naver.com>
2023-05-31 07:45:06 +0900

    함수 옮기기

--- refactoring/chap11/11.4/ex2.js
+++ refactoring/chap11/11.4/ex2.js
@@ -1,13 +1,15 @@
 function example(){
     const tempRange = aRoom.daysTempRange;
-    const isWithinRange = xxNEWwithinRange();
-    //함수 추출하기
-    function xxNEWwithinRange() {
+    const isWithinRange = aPlan.xxNEWwithinRange();
+    if (!isWithinRange)
+            alerts.push("방 온도가 지정 범위를 벗어났습니다.");
+}
+class HeatingPlan{
+    //함수 옮기기
+    xxNEWwithinRange() {
         const low = tempRange.low;
         const high = tempRange.high;
         const isWithinRange = aPlan.withinRange(low, high);
-        if (!isWithinRange)
-            alerts.push("방 온도가 지정 범위를 벗어났습니다.");
         return isWithinRange;
     }
 }
