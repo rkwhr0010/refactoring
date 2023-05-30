3b8112bde7260c8139e993d08a309c6af5d43712
kks <rkwhr0010@naver.com>
2023-05-30 20:10:50 +0900

    완료

--- refactoring/chap11/11.2/ex2.js
+++ refactoring/chap11/11.2/ex2.js
@@ -2,9 +2,6 @@ function baseCharge(usage){
     if(usage < 0) return usd(0);
     const amount = withinBand(usage,0,100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
-        + topBand(usage) * 0.07;
+        + withinBand(usage, 200, Infinity) * 0.07;
     return usd(amount);
-}
-function topBand(usage){
-    return usage > 200 ? usage - 200 : 0;
 }
