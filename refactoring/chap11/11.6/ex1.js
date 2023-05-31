149cdcedd312473391694b106af197747784614d
kks <rkwhr0010@naver.com>
2023-05-31 21:28:14 +0900

    함수 인라인

--- refactoring/chap11/11.6/ex1.js
+++ refactoring/chap11/11.6/ex1.js
@@ -1,9 +1,8 @@
 class HeatingPlan{
     get targetTemperature(){
-        const selectedTemperature = thermostat.selectedTemperature;
-        return this.xxNEWtargetTemperature(selectedTemperature);
+        //함수 인라인
+        return this.xxNEWtargetTemperature(thermostat.selectedTemperature);
     }
-    //함수 추출하기
     xxNEWtargetTemperature(selectedTemperature) {
         if(selectedTemperature > this._max) return this._max;
         else if(selectedTemperature < this._min) return this._min;
@@ -13,7 +12,7 @@ class HeatingPlan{
 
 //호출자
 function caller(){
-    if(thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
-    else if(thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+    if(thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature) setToHeat();
+    else if(thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature) setToCool();
     else setOff();
 }
