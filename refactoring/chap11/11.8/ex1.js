aad95f361e1f9ce1d55769c5f4067380a9b89e85
kks <rkwhr0010@naver.com>
2023-06-01 20:04:45 +0900

    매개변수로 리터럴 악취 제거

--- refactoring/chap11/11.8/ex1.js
+++ refactoring/chap11/11.8/ex1.js
@@ -2,6 +2,7 @@ function createEmployee(name, typeCode){
     return new Employee(name,typeCode);
 }
 
+
 class Employee{
     constructor(name, typeCode){
         this._name = name;
@@ -17,3 +18,9 @@ class Employee{
 }
 //클라이언트, 생성자 호출 코드 대체
 const candidate = createEmployee(document.name, document.empType);
+//메서드 호출에 리터럴을 쓰는 것은 악취다
+const candidate2 = createEmployee(document.name, 'E');
+//팩터리 메서드 이름을 이용해 악취를 제거한다.
+function createEngineer(name){
+    return new Employee(name, "E");
+}
