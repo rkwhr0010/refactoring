5ab5ce06b0c90b63b39ffea77867485aaa20970d
kks <rkwhr0010@naver.com>
2023-06-01 22:06:58 +0900

    완료

--- refactoring/chap11/11.10/ex1.js
+++ refactoring/chap11/11.10/ex1.js
@@ -1,13 +1,6 @@
 function charge(customer, usage, provider){
-    return new ChargeCalculator()
-        .charge(customer, usage, provider);
-}
-class ChargeCalculator{
-    //나머지도 하나씩 적용하면서 테스트 
-    charge(customer, usage, provider){
-        const baseCharge = customer.baseRate * usage;
-        return baseCharge + provider.connectionCharge;
-    }
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
 //호출자...
 const monthCharge = new ChargeCalculator(customer, usage, provider).charge;
