0d7919a2c6986331715bb9c1e7f46614102b838d
kks <rkwhr0010@naver.com>
2023-06-01 22:56:59 +0900

    반환할 변수 선언 후 적절한 이름으로 변경

--- refactoring/chap11/11.11/ex1.js
+++ refactoring/chap11/11.11/ex1.js
@@ -5,11 +5,12 @@ totalAscent = calculateAscent();
 calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
-//값을 반환하도록 수정
+
 function calculateAscent(){
+    let result  = 0; //적절한 이름으로 변경
     for(let i = 1; i<ProcessingInstruction.length;i++){
         const verticalChange = points[i].elevation - points[i-1].elevation;
-        totalAscent += (verticalChange>0) ? verticalChange : 0;
+        result += (verticalChange>0) ? verticalChange : 0;
     }
-    return totalAscent;
+    return result;
 }
