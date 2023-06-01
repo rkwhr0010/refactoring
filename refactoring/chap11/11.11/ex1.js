33345a16e97b8a6374b83a6fb1979656cd522efe
kks <rkwhr0010@naver.com>
2023-06-01 22:55:09 +0900

    값을 반환하도록 수정

--- refactoring/chap11/11.11/ex1.js
+++ refactoring/chap11/11.11/ex1.js
@@ -1,14 +1,15 @@
 let totalAscent = 0;
 let totalTime = 0;
 let totalDistance = 0;
-calculateAscent();
+totalAscent = calculateAscent();
 calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
-
+//값을 반환하도록 수정
 function calculateAscent(){
     for(let i = 1; i<ProcessingInstruction.length;i++){
         const verticalChange = points[i].elevation - points[i-1].elevation;
         totalAscent += (verticalChange>0) ? verticalChange : 0;
     }
+    return totalAscent;
 }
