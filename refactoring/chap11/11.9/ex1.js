9e9b148fc57002e1342765aa56c0e64979daae27
kks <rkwhr0010@naver.com>
2023-06-01 20:52:08 +0900

    명령 매개변수 최대한 생성자로 옮기기

--- refactoring/chap11/11.9/ex1.js
+++ refactoring/chap11/11.9/ex1.js
@@ -1,18 +1,27 @@
 function score(candidate, medicalExam, scoringGuide){
-    let result = 0;
-    let healthLevel = 0;
-    let highMedicalRiskFlag = false;
-
-    if(medicalExam.isSmoker){
-        healthLevel += 10;
-        highMedicalRiskFlag =true;
+    return new Score(candidate).execute(medicalExam,scoringGuide);
+}
+//클래스를 만들고 함수 옮기기
+class Score{
+    constructor(candidate){
+        this._candidate = candidate;
     }
-    let certificationGrade = "regular";
-    if(scoringGuide.stateWithLowCertification(candidate.originState)){
-        certificationGrade = "low";
-        result -= 5;
+    execute(medicalExam, scoringGuide){
+        let result = 0;
+        let healthLevel = 0;
+        let highMedicalRiskFlag = false;
+    
+        if(medicalExam.isSmoker){
+            healthLevel += 10;
+            highMedicalRiskFlag =true;
+        }
+        let certificationGrade = "regular";
+        if(scoringGuide.stateWithLowCertification(candidate.originState)){
+            certificationGrade = "low";
+            result -= 5;
+        }
+        //많은 코드...
+        result -= Math.max(healthLevel -5 , 0);
+        return result;
     }
-    //많은 코드...
-    result -= Math.max(healthLevel -5 , 0);
-    return result;
 }
