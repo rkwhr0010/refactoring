ece3c54205936801d66b66caea43259db1314b5a
kks <rkwhr0010@naver.com>
2023-06-01 21:02:15 +0900

    더 가다듬기, 명령의 목적인 복잡한 함수를 잘게 쪼게기 시작

--- refactoring/chap11/11.9/ex1.js
+++ refactoring/chap11/11.9/ex1.js
@@ -1,29 +1,29 @@
 function score(candidate, medicalExam, scoringGuide){
     return new Score(candidate,medicalExam,scoringGuide).execute();
 }
-//명령 매개변수 최대한 생성자로 옮기기
 class Score{
     constructor(candidate,medicalExam,scoringGuide){
         this._candidate = candidate;
         this._medicalExam = medicalExam;
         this._scoringGuide = scoringGuide;
     }
+    //더 가다듬기, 명령의 목적인 복잡한 함수를 잘게 쪼게기 시작
     execute(){
-        let result = 0;
-        let healthLevel = 0;
-        let highMedicalRiskFlag = false;
+        this._result = 0;
+        this._healthLevel = 0;
+        this._highMedicalRiskFlag = false;
     
         if(this._medicalExam.isSmoker){
-            healthLevel += 10;
-            highMedicalRiskFlag =true;
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag =true;
         }
         let certificationGrade = "regular";
         if(this._scoringGuide.stateWithLowCertification(this._candidate.originState)){
             certificationGrade = "low";
-            result -= 5;
+            this._result -= 5;
         }
         //많은 코드...
-        result -= Math.max(healthLevel -5 , 0);
+        this._result -= Math.max(this._healthLevel -5 , 0);
         return result;
     }
 }
