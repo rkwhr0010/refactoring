ebf3f695611b2ef35f4d2454c0c03ac044b5fc10
kks <rkwhr0010@naver.com>
2023-06-04 19:30:15 +0900

    서브 클래스 오버라이딩

--- refactoring/chap12/12.6/ex1.js
+++ refactoring/chap12/12.6/ex1.js
@@ -4,11 +4,14 @@ class Employee{
         this._name = name;
         this._type = type;
     }
-    //타입 코드 캡슐화
     get tpye(){return this._type;}
     validateType(arg){
         if(!["engineer","manager","salesperson"].includes(arg))
             throw new Error(`${arg}라는 직운 유형은 없습니다.`);
     }
-    toString(){return `${this._name} (${this._type})`;}
+    toString(){return `${this._name} (${this.type})`;}//_ 없음
+}
+//서브 클래스 오버라이딩
+class Engineer extends Employee{
+    get type(){return "engineer";}
 }
