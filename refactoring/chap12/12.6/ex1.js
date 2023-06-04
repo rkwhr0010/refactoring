3192ace2765be9a2b8d87eb79d5d51f1be76537d
kks <rkwhr0010@naver.com>
2023-06-04 19:38:16 +0900

    팩터리 메서드에서 검증은 대체가능 하니 제거

--- refactoring/chap12/12.6/ex1.js
+++ refactoring/chap12/12.6/ex1.js
@@ -3,19 +3,14 @@ function createEmployee(name, type){
         case "engineer" : return new Engineer(name,type);
         case "salesperson" : return new Salesperson(name,type);
         case "manager" : return new Manager(name,type);
+        default : throw new Error(`${arg}라는 직운 유형은 없습니다.`);
     }
-    return new Employee(name, type);
 }
 
 class Employee{
-    //모든 서브클래스에서 재정의한 필드와 게터제거
+    //팩터리 메서드에서 검증은 대체가능 하니 제거
     constructor(name, type){
-        this.validateType(type);
         this._name = name;
-    }
-    validateType(arg){
-        if(!["engineer","manager","salesperson"].includes(arg))
-            throw new Error(`${arg}라는 직운 유형은 없습니다.`);
     }
     toString(){return `${this._name} (${this.type})`;}
 }
