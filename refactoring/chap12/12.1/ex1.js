85fd114a68d7a8b0e8351254775cbe20811824d2
kks <rkwhr0010@naver.com>
2023-06-04 16:08:30 +0900

    서브 클래스 중 하나로 이름을 통일하고, 슈퍼 클래스에 메서드를 복사한다.

--- refactoring/chap12/12.1/ex1.js
+++ refactoring/chap12/12.1/ex1.js
@@ -1,14 +1,16 @@
 class Party{
-
+    get annualCost(){
+        return this.monthlyCost *12 ;
+    }
 }
-//서브클래스
+//서브 클래스 중 하나로 이름을 통일하고, 슈퍼 클래스에 메서드를 복사한다.
 class Employee extends Party{
     get annualCost(){
         return this.monthlyCost *12 ;
     }
 }
 class Department extends Party{
-    get totalAnnualCost(){
+    get annualCost(){
         return this.monthlyCost *12 ;
     }
 }
