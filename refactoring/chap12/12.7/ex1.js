8157c86e97dc6f2cd06197fd9321c738ed2fb585
kks <rkwhr0010@naver.com>
2023-06-04 22:15:47 +0900

    직관적으로 생성자 마다 팩터리 메서드를 만들 경우

--- refactoring/chap12/12.7/ex1.js
+++ refactoring/chap12/12.7/ex1.js
@@ -1,3 +1,30 @@
+//직관적으로 생성자 마다 팩터리 메서드를 만들 경우
+function createPerson(name){
+    return new Person(name);
+}
+function createMale(name){
+    return new Male(name);
+}
+function createFemale(name){
+    return new Female(name);
+}
+//결국, 리팩터링하면 큰 의미가 없다.
+function loadFromInput(data){
+    const result = [];
+    data.forEach(aRecord=>{
+        let p;
+        switch (aRecord.gender){
+            case 'M' : p = new Male(aRecord.name); break;
+            case 'F' : p = new Female(aRecord.name); break;
+            default : p = new Person(aRecord.name); 
+        }
+        result.push(p);
+    });
+    return result;
+}
+
+
+
 class Person{
     constructor(name){
         this._name = name;
@@ -11,3 +38,6 @@ class Male extends Person{
 class Female extends Person{
     get genderCode(){return "F";}
 }
+
+//클라이언트
+const numberOfMales = people.filter(p=>p instanceof Male).length;
