4b4bacb044a4b3a778cf58aa0770da5c32a559b1
kks <rkwhr0010@naver.com>
2023-06-04 17:19:15 +0900

    먼저 공통 코드를 함수로 추출

--- refactoring/chap12/12.3/ex2.js
+++ refactoring/chap12/12.3/ex2.js
@@ -4,13 +4,18 @@ class Employee{
     get isPrivileged(){}
     assignCar(){}
 }
-//예시 : 공통 코드가 나중에 올때
 class Manager extends Employee{
     constructor(name, grade){
         super(name);
         this._grade = grade;
-        if(this.isPrivileged) this.assignCar();
+        this.finishConstruction();
     }
+    //먼저 공통 코드를 함수로 추출
+    finishConstruction() {
+        if (this.isPrivileged)
+            this.assignCar();
+    }
+
     get isPrivileged(){
         return this._grade >4;
     }
