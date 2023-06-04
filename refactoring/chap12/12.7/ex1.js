8b1735119f05025497682e24395578edcde33f07
kks <rkwhr0010@naver.com>
2023-06-04 22:23:37 +0900

    클라이언트에서 instanceof 연산자사용 함수로 캡슐화

--- refactoring/chap12/12.7/ex1.js
+++ refactoring/chap12/12.7/ex1.js
@@ -1,4 +1,3 @@
-//두 함수 다듬기 
 function createPerson(aRecord){
     switch (aRecord.gender){
         case 'M' : return new Male(aRecord.name); 
@@ -6,17 +5,15 @@ function createPerson(aRecord){
         default : return new Person(aRecord.name); 
     }
 }
-//반복문 파이프라인으로 바꾸기
 function loadFromInput(data){
     return data.map(aRecord=>createPerson(aRecord));
 }
-
-
-
 class Person{
     constructor(name){
         this._name = name;
     }
+    //타입 검사 코드를 함수로 추출
+    get isMale(){return this instanceof Male;}
     get name(){return this._name;}
     get genderCode(){return "X";}
 }
@@ -28,4 +25,5 @@ class Female extends Person{
 }
 
 //클라이언트
-const numberOfMales = people.filter(p=>p instanceof Male).length;
+//클라이언트에서 instanceof 연산자를 사용하는 것부터 구린내가 난다.
+const numberOfMales = people.filter(p=>p.isMale).length;
