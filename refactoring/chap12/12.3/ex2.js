8847045fa21e52b3c8da569a5884191309ae8e76
kks <rkwhr0010@naver.com>
2023-06-04 17:20:39 +0900

    슈퍼 클래스로 옮김

--- refactoring/chap12/12.3/ex2.js
+++ refactoring/chap12/12.3/ex2.js
@@ -3,6 +3,11 @@ class Employee{
     }
     get isPrivileged(){}
     assignCar(){}
+    //슈퍼클래스로 옮김
+    finishConstruction() {
+        if (this.isPrivileged)
+            this.assignCar();
+    }
 }
 class Manager extends Employee{
     constructor(name, grade){
@@ -10,12 +15,6 @@ class Manager extends Employee{
         this._grade = grade;
         this.finishConstruction();
     }
-    //먼저 공통 코드를 함수로 추출
-    finishConstruction() {
-        if (this.isPrivileged)
-            this.assignCar();
-    }
-
     get isPrivileged(){
         return this._grade >4;
     }
