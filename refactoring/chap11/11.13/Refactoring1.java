3f611577aeebdaf812a38a2e1941cc33a837f462
kks <rkwhr0010@naver.com>
2023-06-04 15:50:51 +0900

    예외를 사전 확인으로 변경

--- refactoring/chap11/11.13/Refactoring1.java
+++ refactoring/chap11/11.13/Refactoring1.java
@@ -14,12 +14,17 @@ public class Refactoring1 {
 		
 		public Resource get() {
 			Resource result;
-			try {
-				result = available.pop();
-				allocated.add(result);
-			}catch (NoSuchElementException e) {
+			//풀에서 자원이 고갈되는 건 예상치 못한 조건이 아니다. 따라서 예외처리로 대응하는 건 옳지 않다.
+			if(available.isEmpty()) {
 				result = Resource.create();
 				allocated.add(result);
+			}else {
+				try {
+					result = available.pop();
+					allocated.add(result);
+				}catch (NoSuchElementException e) {
+					throw new AssertionError("도달 불가");
+				}
 			}
 			return result;
 		}
