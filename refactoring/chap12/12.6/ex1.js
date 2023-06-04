9ac013b56b8a9ce3d601d240fa712b903cfd4021
kks <rkwhr0010@naver.com>
2023-06-04 19:35:07 +0900

    나머지 서브클래스에도 적용

--- refactoring/chap12/12.6/ex1.js
+++ refactoring/chap12/12.6/ex1.js
@@ -1,7 +1,8 @@
-//생성자를 팩터리 함수로 대체
 function createEmployee(name, type){
     switch(tpye){
         case "engineer" : return new Engineer(name,type);
+        case "salesperson" : return new Salesperson(name,type);
+        case "manager" : return new Manager(name,type);
     }
     return new Employee(name, type);
 }
@@ -17,9 +18,14 @@ class Employee{
         if(!["engineer","manager","salesperson"].includes(arg))
             throw new Error(`${arg}라는 직운 유형은 없습니다.`);
     }
-    toString(){return `${this._name} (${this.type})`;}//_ 없음
+    toString(){return `${this._name} (${this.type})`;}
 }
-//서브 클래스 오버라이딩
 class Engineer extends Employee{
     get type(){return "engineer";}
+}
+class Salesperson extends Employee{
+    get type(){return "salesperson";}
+}
+class Manager extends Employee{
+    get type(){return "manager";}
 }
