ce666ff68bf347e27f73fa335b8fb7a81fb48226
kks <rkwhr0010@naver.com>
2023-06-04 20:17:34 +0900

    똑같이 리팩터링 진행

--- refactoring/chap12/12.6/ex2.js
+++ refactoring/chap12/12.6/ex2.js
@@ -1,23 +1,30 @@
-//타입 코드를 클래스로 만들기(기본형을 객체로 만듦)
 class EmployeeType{
-    constructor(aString){
-        this._value = aString;
-    }
-    toString(){return this._value;}
+}
+class Engineer extends EmployeeType{
+    toString(){return "engineer";}
+}
+class Salesperson extends EmployeeType{
+    toString(){return "salesperson";}
+}
+class Manager extends EmployeeType{
+    toString(){return "manager";}
 }
 class Employee{
     constructor(name,type){
-        this.validateType(type);
         this._name = name;
         this._type = type;
     }
-    validateType(arg){
-        if(!["engineer","manager","salesperson"].includes(arg))
-            throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    static createEmployeeType(aString){
+        switch(aString){
+            case "engineer" : return new Engineer();
+            case "salesperson" : return new Salesperson();
+            case "manager" : return new Manager();
+            default : throw new Error(`${aString}라는 직운 유형은 없습니다.`);
+        }
     }
     get typeString() {return this._type.toString();}
     get type(){return this._type;}
-    set type(arg){this._type = arg;}
+    set type(arg){this._type = Employee.createEmployeeType(arg);}
 
     get capitalizedType(){
         return this.typeString.charAt(0).toUpperCase() 
