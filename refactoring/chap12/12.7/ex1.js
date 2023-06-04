7d9c779b261ae60069edb47f2377d251c10de7f9
kks <rkwhr0010@naver.com>
2023-06-04 22:30:04 +0900

    완료

--- refactoring/chap12/12.7/ex1.js
+++ refactoring/chap12/12.7/ex1.js
@@ -1,26 +1,21 @@
 function createPerson(aRecord){
     switch (aRecord.gender){
-        //하나씩 서브클래스 제거
         case 'M' : return new Person(aRecord.name, "M"); 
-        case 'F' : return new Female(aRecord.name); 
-        default : return new Person(aRecord.name); 
+        case 'F' : return new Person(aRecord.name, "F"); 
+        default : return new Person(aRecord.name, "X"); 
     }
 }
 function loadFromInput(data){
     return data.map(aRecord=>createPerson(aRecord));
 }
 class Person{
-    //생성자에 젠더코드 추가
     constructor(name, genderCode){
         this._name = name;
-        this._genderCode = genderCode || "X";
+        this._genderCode = genderCode ;
     }
     get isMale(){return "M"=== this._genderCode;}
     get name(){return this._name;}
     get genderCode(){return this._genderCode;}
-}
-class Female extends Person{
-    get genderCode(){return "F";}
 }
 
 //클라이언트
