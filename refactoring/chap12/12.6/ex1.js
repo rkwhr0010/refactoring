47e8b2435ca91e288811fb6ac878e2fac2a03312
kks <rkwhr0010@naver.com>
2023-06-04 19:40:25 +0900

    필요없어진 타입 코드 인자 제거

--- refactoring/chap12/12.6/ex1.js
+++ refactoring/chap12/12.6/ex1.js
@@ -1,17 +1,18 @@
-function createEmployee(name, type){
+//필요없어진 타입 코드 인자 제거
+function createEmployee(name){
     switch(tpye){
-        case "engineer" : return new Engineer(name,type);
-        case "salesperson" : return new Salesperson(name,type);
-        case "manager" : return new Manager(name,type);
+        case "engineer" : return new Engineer(name);
+        case "salesperson" : return new Salesperson(name);
+        case "manager" : return new Manager(name);
         default : throw new Error(`${arg}라는 직운 유형은 없습니다.`);
     }
 }
 
 class Employee{
-    //팩터리 메서드에서 검증은 대체가능 하니 제거
-    constructor(name, type){
+    constructor(name){
         this._name = name;
     }
+    //서브 클래스에는 아직 type 코드 게터가 있다.
     toString(){return `${this._name} (${this.type})`;}
 }
 class Engineer extends Employee{
