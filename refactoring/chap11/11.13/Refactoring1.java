c9beb9f5911abff94b144616c66f768dc937267b
kks <rkwhr0010@naver.com>
2023-06-04 15:51:54 +0900

    예외에 도달하지 못한다면 제거한다.

--- refactoring/chap11/11.13/Refactoring1.java
+++ refactoring/chap11/11.13/Refactoring1.java
@@ -1,6 +1,5 @@
 import java.util.Deque;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public class Refactoring1 {
 	static class Resource{
@@ -14,17 +13,12 @@ public class Refactoring1 {
 		
 		public Resource get() {
 			Resource result;
-			//풀에서 자원이 고갈되는 건 예상치 못한 조건이 아니다. 따라서 예외처리로 대응하는 건 옳지 않다.
 			if(available.isEmpty()) {
 				result = Resource.create();
 				allocated.add(result);
 			}else {
-				try {
-					result = available.pop();
-					allocated.add(result);
-				}catch (NoSuchElementException e) {
-					throw new AssertionError("도달 불가");
-				}
+				result = available.pop();
+				allocated.add(result);
 			}
 			return result;
 		}
