0efe4900d2017d44cabbf698a152def3a9d08773
kks <rkwhr0010@naver.com>
2023-06-04 19:26:14 +0900

    타입 코드 캡슐화

--- refactoring/chap12/12.6/ex1.js
+++ refactoring/chap12/12.6/ex1.js
@@ -4,6 +4,8 @@ class Employee{
         this._name = name;
         this._type = type;
     }
+    //타입 코드 캡슐화
+    get tpye(){return this._type;}
     validateType(arg){
         if(!["engineer","manager","salesperson"].includes(arg))
             throw new Error(`${arg}라는 직운 유형은 없습니다.`);
