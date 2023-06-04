9da84eb3e92dacc84abc350b825d3862fc89d45b
kks <rkwhr0010@naver.com>
2023-06-04 14:33:25 +0900

    별도 추가된 예외만들 처리하는 경우 처리방식

--- refactoring/chap11/11.12/ex1.js
+++ refactoring/chap11/11.12/ex1.js
@@ -11,8 +11,23 @@ function calculateShippingCosts(anOrder){
     //코드들...
 }
 //최상위
-let status; //스코프 문제로 선언과 초기화를 어쩔 수 없이 분리
+let status; 
 try {
     status = calculateShippingCosts(orderData);
-} catch (error) {/*예외처리*/}
+} catch (error) {
+    if(error instanceof OrderProcessingError)
+        errorList.push({order:orderData, errorCode:statuts});
+    else throw error;
+    throw error;
+}
+
 if(status < 0) errorList.push({order:orderData, errorCode:statuts});
+
+//별도 추가된 예외만들 처리하는 경우 처리방식
+class OrderProcessingError extends Error{
+    constructor(errorCode){
+        super(`주문 처리 오류 : ${errorCode}`);
+        this.code = errorCode;
+    }
+    get name(){return "OrderProcessingError";}
+}
