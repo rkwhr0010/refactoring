5889a2801a43cca3b42286b39af9b78efb08b0fd
kks <rkwhr0010@naver.com>
2023-06-05 20:42:10 +0900

    옮긴 데이터 관련 메서드 올리기

--- refactoring/chap12/12.8/ex1.js
+++ refactoring/chap12/12.8/ex1.js
@@ -1,22 +1,26 @@
-class Party{} //일단 빈 슈퍼클래스를 만들고 확장한다.
+class Party{
+    constructor(name){
+        this._name = name;
+    }
+    //옮긴 데이터 관련 메서드 올리기
+    get name() {return this._name;}
+} 
 class Employee extends Party{
     constructor(name, id, monthlyCost){
+        super(name);
         this._id = id;
-        this._name = name;
         this._monthlyCost = monthlyCost;
     }
     get monthlyCost() {return this._monthlyCost;}
-    get name() {return this._name;}
     get id() {return this._id;}
     get annualCost(){ return this._monthlyCost * 12; }
 }
 class Department extends Party{
     constructor(name, staff){
-        this._name = name;
+        super(name);
         this._staff = staff;
     }
     get staff(){return this._staff.slice();}
-    get name(){return this._name;}
     get totalMonthlyCost(){
         return this.staff
             .map(e=e.monthlyCost)
