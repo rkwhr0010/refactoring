286ef65708d9f11c84f9a929c479b85964f11553
kks <rkwhr0010@naver.com>
2023-06-05 20:55:13 +0900

    완료

--- refactoring/chap12/12.9/ex1.js
+++ refactoring/chap12/12.9/ex1.js
@@ -1,6 +1,9 @@
 class Super{
     common(){/**슈퍼클래스 작업... */    }
+    specific(){/**서브클래스 작업... */}
 }
-class Sub extends Super{
+//위로 합치던가, 아래로 합치던가 판단하여 적용
+class Sub{
+    common(){/**슈퍼클래스 작업... */    }
     specific(){/**서브클래스 작업... */}
 }
