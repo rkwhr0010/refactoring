e2750cac0c05ded4df5f5b34d3a79742bf8583b5
kks <rkwhr0010@naver.com>
2023-06-06 16:20:12 +0900

    더 다름기: 값을 참조로 바꾸기 리팩터링

--- refactoring/chap12/12.11/ex1.js
+++ refactoring/chap12/12.11/ex1.js
@@ -8,14 +8,13 @@ class CatalogItem{
     get title(){return this._title;}
     hasTag(arg){return this._tags.includes(arg);}
 }
-class Scroll{//상속관계를 끊는다.
+class Scroll{
     constructor(id, title, tags, dateLastCleaned){
-        //슈퍼 클래스 참조를 하나 만들고 슈퍼클래스를 생성한다.
-        this._catalogItem = new CatalogItem(id, title, tags);
+        this._id = id;//먼저 id를 내 속성으로 만들기
+        this._catalogItem = new CatalogItem(null, title, tags);
         this._lastCleaned = dateLastCleaned;
     }
-    //슈퍼 클래스에 대응하는 메서드를 만들고, 전달 메서드화 시킨다.
-    get id(){return this._catalogItem._id;}
+    get id(){return this._id;}
     get title(){return this._catalogItem._title;}
     hasTag(arg){return this._catalogItem._tags.includes(arg);}
     needsCleaning(targetDate){
